2f1c05ea5966b13b9afb873ae7b7f7cb963c81c7
Roberto Doering <rbydoering@gmail.com>
2018-12-21 16:22:03 +0100

    Add translation button and text to tweet tile

--- lib/components/shared/tweet_tile.dart
+++ lib/components/shared/tweet_tile.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:harpy/api/translate/translate_service.dart';
 import 'package:harpy/api/twitter/data/tweet.dart';
 import 'package:harpy/api/twitter/data/user.dart';
 import 'package:harpy/components/screens/user_profile/user_profile_screen.dart';
@@ -37,10 +36,10 @@ class TweetTile extends StatelessWidget {
           children: <Widget>[
             _buildRetweetedRow(),
             _buildNameRow(context),
-            SizedBox(height: 8.0),
             _buildText(),
+            _buildTranslation(context),
             _buildMedia(),
-            _buildActionRow(),
+            _buildActionRow(context),
           ],
         ),
       ),
@@ -98,18 +97,61 @@ class TweetTile extends StatelessWidget {
   }
 
   Widget _buildText() {
-    return TwitterText(
-      text: tweet.full_text,
-      entities: tweet.entities,
-      onEntityTap: (model) {
-        if (model.type == EntityType.url) {
-          launchUrl(model.url);
-        }
-      },
+    return !tweet.emptyText
+        ? Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: TwitterText(
+              text: tweet.full_text,
+              entities: tweet.entities,
+              onEntityTap: (model) {
+                if (model.type == EntityType.url) {
+                  launchUrl(model.url);
+                }
+              },
+            ),
+          )
+        : Container();
+  }
+
+  Widget _buildTranslation(BuildContext context) {
+    if (tweet.harpyData?.translation == null ||
+        tweet.harpyData.translation.unchanged) {
+      return Container();
+    }
+
+    // todo: expand into existence
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 8.0),
+        Row(
+          children: <Widget>[
+            Text(
+              "Translated from ",
+              style: Theme.of(context).textTheme.display1,
+            ),
+            Text(tweet.harpyData.translation.language),
+          ],
+        ),
+        SizedBox(height: 4.0),
+        TwitterText(
+          text: tweet.harpyData.translation.text,
+          entities: tweet.entities,
+        ),
+      ],
     );
   }
 
-  Widget _buildActionRow() {
+  Widget _buildMedia() {
+    return tweet.extended_entities?.media != null
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: CollapsibleMedia(tweet: tweet),
+          )
+        : Container();
+  }
+
+  Widget _buildActionRow(BuildContext context) {
     return Row(
       children: <Widget>[
         // retweet action
@@ -117,7 +159,7 @@ class TweetTile extends StatelessWidget {
           active: tweet.retweeted,
           inactiveIcon: Icons.repeat,
           activeIcon: Icons.repeat,
-          text: "${tweet.retweetCount}",
+          text: "${formatNumber(tweet.retweetCount)}",
           color: Colors.green,
           activate: () => HomeStore.retweetTweet(tweet),
           deactivate: () => HomeStore.unretweetTweet(tweet),
@@ -128,7 +170,7 @@ class TweetTile extends StatelessWidget {
           active: tweet.favorited,
           inactiveIcon: Icons.favorite_border,
           activeIcon: Icons.favorite,
-          text: "${tweet.favoriteCount}",
+          text: "${formatNumber(tweet.favoriteCount)}",
           color: Colors.red,
           activate: () => HomeStore.favoriteTweet(tweet),
           deactivate: () => HomeStore.unfavoriteTweet(tweet),
@@ -136,28 +178,42 @@ class TweetTile extends StatelessWidget {
 
         Expanded(child: Container()),
 
-        HarpyButton(
-          icon: Icons.translate,
-          onPressed: () {
-            translate(text: tweet.full_text).then(print);
-          },
-          iconColor: Colors.blue,
-          splashColor: Colors.blue,
-          drawColorOnHighlight: true,
-        )
+        _buildTranslationButton(context),
       ],
     );
   }
 
-  Widget _buildMedia() {
-    if (tweet.extended_entities?.media != null) {
-      return Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: CollapsibleMedia(tweet: tweet),
-      );
-    } else {
+  Widget _buildTranslationButton(BuildContext context) {
+    if (tweet.emptyText || tweet.lang == "en") {
       return Container();
     }
+
+    VoidCallback onPressed;
+    bool drawColorOnHighlight = false;
+    Color color = Colors.blue;
+
+    if (tweet.harpyData.translation == null) {
+      drawColorOnHighlight = true;
+      onPressed = () async {
+        await HomeStore.translateTweet(tweet);
+
+        if (tweet.harpyData.translation.unchanged) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text("Tweet not translated"),
+          ));
+        }
+      };
+    } else if (tweet.harpyData.translation.unchanged) {
+      color = Theme.of(context).disabledColor;
+    }
+
+    return HarpyButton(
+      icon: Icons.translate,
+      onPressed: onPressed,
+      iconColor: color,
+      splashColor: color,
+      drawColorOnHighlight: drawColorOnHighlight,
+    );
   }
 
   void _openUserProfile(BuildContext context) {
