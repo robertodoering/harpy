d6c855a52bd4e499cdad81ae6a6dac2e0aab8dec
Roberto Doering <rbydoering@gmail.com>
2019-06-28 17:16:00 +0200

    Update custom theme data

--- lib/core/shared_preferences/theme/harpy_theme_data.g.dart
+++ lib/core/shared_preferences/theme/harpy_theme_data.g.dart
@@ -8,9 +8,17 @@ part of 'harpy_theme_data.dart';
 
 HarpyThemeData _$HarpyThemeDataFromJson(Map<String, dynamic> json) {
   return HarpyThemeData()
+    ..name = json['name'] as String
     ..backgroundColors =
-        (json['backgroundColors'] as List)?.map((e) => e as int)?.toList();
+        (json['backgroundColors'] as List)?.map((e) => e as int)?.toList()
+    ..primaryColor = json['primaryColor'] as int
+    ..accentColor = json['accentColor'] as int;
 }
 
 Map<String, dynamic> _$HarpyThemeDataToJson(HarpyThemeData instance) =>
-    <String, dynamic>{'backgroundColors': instance.backgroundColors};
+    <String, dynamic>{
+      'name': instance.name,
+      'backgroundColors': instance.backgroundColors,
+      'primaryColor': instance.primaryColor,
+      'accentColor': instance.accentColor
+    };
