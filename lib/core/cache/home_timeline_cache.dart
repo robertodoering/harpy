434bf0af183c3f7d74ee3553c264609d4d5bf042
Roberto Doering <rbydoering@gmail.com>
2019-07-27 17:06:30 +0200

    Remove file cache & isolate work helper function

--- lib/core/cache/home_timeline_cache.dart
+++
@@ -1,9 +0,0 @@
-import 'package:harpy/core/cache/tweet_cache.dart';
-
-class HomeTimelineCache extends TweetCache {
-  HomeTimelineCache() {
-    data.type = homeTimeline;
-  }
-
-  static const String homeTimeline = "home_timeline";
-}
