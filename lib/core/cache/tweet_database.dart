fe0d111ad047d166e068413b6cabf56df2975f9d
Roberto Doering <rbydoering@gmail.com>
2019-07-31 21:35:31 +0200

    Change tweet database

--- lib/core/cache/tweet_database.dart
+++ lib/core/cache/tweet_database.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:harpy/api/twitter/data/tweet.dart';
 import 'package:harpy/core/cache/database.dart';
+import 'package:harpy/core/utils/json_utils.dart';
 import 'package:logging/logging.dart';
 import 'package:sembast/sembast.dart';
 
-/// Stores [Tweet] objects as a json object in a database.
+/// Stores [Tweet] objects in a database.
 class TweetDatabase extends HarpyDatabase {
   TweetDatabase({
     StoreRef<int, Map<String, dynamic>> store,
@@ -14,20 +16,57 @@ class TweetDatabase extends HarpyDatabase {
   @override
   String get name => "tweet_db/$subDirectory";
 
-  final Logger _log = Logger("TweetDatabase");
+  static final Logger _log = Logger("TweetDatabase");
 
-  /// Records or the [tweet] in the tweet database.
+  /// Records the [tweet] in the tweet database.
   ///
   /// Overrides any existing [Tweet] with the same [Tweet.id].
   Future<bool> recordTweet(Tweet tweet) async {
     _log.fine("recording tweet with id ${tweet.id}");
 
     try {
-      await store.record(tweet.id).put(db, tweet.toJson());
+      final Map<String, dynamic> tweetJson =
+          await compute<Tweet, Map<String, dynamic>>(
+        _handleTweetSerialization,
+        tweet,
+      );
+
+      await store.record(tweet.id).put(db, tweetJson);
+
       _log.fine("tweet recorded");
       return true;
     } catch (e, st) {
       _log.severe("error while trying to record tweet", e, st);
+      return false;
+    }
+  }
+
+  /// Records all [tweets] in the database in a single transaction.
+  ///
+  /// Any existing [Tweet] with the same [Tweet.id] will be overridden.
+  Future<bool> recordTweetList(List<Tweet> tweets) async {
+    _log.fine("recording ${tweets.length} tweets");
+
+    try {
+      final List<Map<String, dynamic>> tweetJsonList =
+          await compute<List<Tweet>, List<Map<String, dynamic>>>(
+        _handleTweetListSerialization,
+        tweets,
+      );
+
+      await db.transaction((transaction) async {
+        for (int i = 0; i < tweetJsonList.length; i++) {
+          final Tweet tweet = tweets[i];
+          final Map<String, dynamic> json = tweetJsonList[i];
+
+          await store.record(tweet.id).put(transaction, json);
+        }
+      });
+
+      _log.fine("tweets recorded");
+      return true;
+    } catch (e, st) {
+      _log.severe("error while trying to record tweets", e, st);
       return false;
     }
   }
@@ -40,31 +79,58 @@ class TweetDatabase extends HarpyDatabase {
   Future<List<Tweet>> findTweets(List<int> ids) async {
     _log.fine("finding ${ids.length} tweets");
 
-    final records = await store.find(
-      db,
-      finder: Finder(
-        filter: Filter.inList("id", ids),
-        sortOrders: [SortOrder("id", false, true)],
-      ),
-    );
+    try {
+      final records = await store.find(
+        db,
+        finder: Finder(
+          filter: Filter.inList("id", ids),
+          sortOrders: [SortOrder("id", false, true)],
+        ),
+      );
 
-    final tweets =
-        records.map((record) => Tweet.fromJson(record.value)).toList();
+      final List<Tweet> tweets = await compute<
+          List<RecordSnapshot<int, Map<String, dynamic>>>, List<Tweet>>(
+        _handleTweetListDeserialization,
+        records,
+      );
 
-    _log.fine("found ${tweets.length} tweets");
+      _log.fine("found ${tweets.length} tweets");
 
-    return tweets;
+      return tweets;
+    } catch (e, st) {
+      _log.severe("exception while finding tweets", e, st);
+      return <Tweet>[];
+    }
   }
 
   /// Returns whether or not a tweet with the [id] exists in the tweet database.
   Future<bool> tweetExists(int id) async {
     _log.fine("checking if tweet with id $id exists");
 
-    final record = await store.findFirst(
-      db,
-      finder: Finder(filter: Filter.byKey(id)),
-    );
+    try {
+      final record = await store.findFirst(
+        db,
+        finder: Finder(filter: Filter.byKey(id)),
+      );
 
-    return record != null;
+      return record != null;
+    } catch (e, st) {
+      _log.severe("exception while checking if tweet exists", e, st);
+      return false;
+    }
   }
+}
+
+List<Map<String, dynamic>> _handleTweetListSerialization(List<Tweet> tweets) {
+  return tweets.map((tweet) => toPrimitiveJson(tweet.toJson())).toList();
+}
+
+Map<String, dynamic> _handleTweetSerialization(Tweet tweet) {
+  return toPrimitiveJson(tweet.toJson());
+}
+
+List<Tweet> _handleTweetListDeserialization(
+  List<RecordSnapshot<int, Map<String, dynamic>>> records,
+) {
+  return records.map((record) => Tweet.fromJson(record.value)).toList();
 }
