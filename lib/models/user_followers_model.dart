b80bbb806078d0a1f597a767f9d72647a959cc23
Roberto Doering <rbydoering@gmail.com>
2019-09-23 20:06:06 +0200

    Refactor the user following & follwer model to use a common mixin

--- lib/models/user_followers_model.dart
+++ lib/models/user_followers_model.dart
@@ -1,5 +1,3 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:harpy/api/twitter/data/paginated_users.dart';
 import 'package:harpy/api/twitter/data/user.dart';
@@ -7,11 +5,13 @@ import 'package:harpy/api/twitter/error_handler.dart';
 import 'package:harpy/api/twitter/services/user_service.dart';
 import 'package:harpy/harpy.dart';
 import 'package:harpy/models/paginated_model.dart';
+import 'package:harpy/models/user_following_model.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
 /// The model for showing the followers for a [User].
-class UserFollowersModel extends ChangeNotifier with PaginatedModel {
+class UserFollowersModel extends ChangeNotifier
+    with PaginatedModel, UserFollowingFollowersModel {
   UserFollowersModel({
     @required this.userId,
   }) {
@@ -29,33 +29,42 @@ class UserFollowersModel extends ChangeNotifier with PaginatedModel {
 
   static final Logger _log = Logger("UserFollowersModel");
 
-  /// The list of found followers.
-  ///
-  /// This list will grow as more pages are requested.
-  final List<User> _users = [];
-  List<User> get users => UnmodifiableListView(_users);
-
-  /// Determines what page is being returned for the [UserService.getFollowers]
-  /// request.
-  ///
-  /// `-1` represents the first page.
-  int _nextCursor = -1;
-
   /// Uses the [UserService] to search for the followers of the [User]
   /// corresponding to the [userId].
   @override
   Future<void> search() async {
     final PaginatedUsers paginatedUsers = await userService
-        .getFollowers(id: userId, cursor: _nextCursor)
+        .getFollowers(id: userId, cursor: nextCursor)
         .catchError(twitterClientErrorHandler);
 
     if (paginatedUsers != null) {
       _log.fine("found ${paginatedUsers.users.length} users");
 
-      _users.addAll(paginatedUsers.users);
-      _nextCursor = paginatedUsers.nextCursor;
-    }
+      users.addAll(paginatedUsers.users);
+      nextCursor = paginatedUsers.nextCursor;
 
-    lastPage = paginatedUsers?.lastPage ?? true;
+      lastPage = paginatedUsers.lastPage || paginatedUsers.users.isEmpty;
+    } else {
+      lastPage = true;
+    }
   }
+}
+
+/// Used by the [UserFollowersModel] and [UserFollowingModel] to hold common
+/// logic.
+mixin UserFollowingFollowersModel on PaginatedModel {
+  /// The list of found users.
+  ///
+  /// This list will grow as more pages are requested.
+  final List<User> users = [];
+
+  /// Whether or not no users have been found.
+  bool get noUsersFound => !loading && users.isEmpty;
+
+  /// Determines what page is being returned for the [UserService.getFollowers]
+  /// or the [UserService.getFollowing] request.
+  ///
+  /// `-1` represents the first page.
+  @protected
+  int nextCursor = -1;
 }
--- lib/models/user_following_model.dart
+++ lib/models/user_following_model.dart
@@ -1,5 +1,3 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:harpy/api/twitter/data/paginated_users.dart';
 import 'package:harpy/api/twitter/data/user.dart';
@@ -7,11 +5,13 @@ import 'package:harpy/api/twitter/error_handler.dart';
 import 'package:harpy/api/twitter/services/user_service.dart';
 import 'package:harpy/harpy.dart';
 import 'package:harpy/models/paginated_model.dart';
+import 'package:harpy/models/user_followers_model.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
 /// The model for showing the following users for a [User].
-class UserFollowingModel extends ChangeNotifier with PaginatedModel {
+class UserFollowingModel extends ChangeNotifier
+    with PaginatedModel, UserFollowingFollowersModel {
   UserFollowingModel({
     @required this.userId,
   }) {
@@ -29,31 +29,21 @@ class UserFollowingModel extends ChangeNotifier with PaginatedModel {
 
   static final Logger _log = Logger("UserFollowersModel");
 
-  /// The list of found followers.
-  ///
-  /// This list will grow as more pages are requested.
-  final List<User> _users = [];
-  List<User> get users => UnmodifiableListView(_users);
-
-  /// Determines what page is being returned for the [UserService.getFollowing]
-  /// request.
-  ///
-  /// `-1` represents the first page.
-  int _nextCursor = -1;
-
   @override
   Future<void> search() async {
     final PaginatedUsers paginatedUsers = await _userService
-        .getFollowing(id: userId, cursor: _nextCursor)
+        .getFollowing(id: userId, cursor: nextCursor)
         .catchError(twitterClientErrorHandler);
 
     if (paginatedUsers != null) {
       _log.fine("found ${paginatedUsers.users.length} users");
 
-      _users.addAll(paginatedUsers.users);
-      _nextCursor = paginatedUsers.nextCursor;
-    }
+      users.addAll(paginatedUsers.users);
+      nextCursor = paginatedUsers.nextCursor;
 
-    lastPage = paginatedUsers?.lastPage ?? true;
+      lastPage = paginatedUsers.lastPage || paginatedUsers.users.isEmpty;
+    } else {
+      lastPage = true;
+    }
   }
 }
