514a55bb85abac5ed3881d457507bc7010f3bd6a
Roberto Doering <rbydoering@gmail.com>
2020-06-12 19:40:44 +0200

    Cleanup

--- lib/components/common/global_bloc_provider.dart
+++ lib/components/common/global_bloc_provider.dart
@@ -3,6 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harpy/components/application/bloc/application/application_bloc.dart';
 import 'package:harpy/components/authentication/bloc/authentication/authentication_bloc.dart';
 
+/// The [GlobalBlocProvider] is built above the root [MaterialApp] to provide
+/// every descendant with globally available blocs.
+///
+/// These blocs will only be created once.
 class GlobalBlocProvider extends StatelessWidget {
   const GlobalBlocProvider({
     @required this.child,
@@ -20,6 +24,7 @@ class GlobalBlocProvider extends StatelessWidget {
         ),
 
         // application
+        // can't be lazy since initialization starts upon bloc creation
         BlocProvider<ApplicationBloc>(
           lazy: false,
           create: (BuildContext context) => ApplicationBloc(
