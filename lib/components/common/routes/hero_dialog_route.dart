31f9b0a2ef0dd7a54b2cf2c9443738dc12ce0bda
Roberto Doering <rbydoering@gmail.com>
2020-07-25 22:08:32 +0200

    Add hero dialog route

---
+++ lib/components/common/routes/hero_dialog_route.dart
@@ -0,0 +1,83 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+/// A [PageRoute] with a semi transparent background.
+///
+/// Similar to calling [showDialog] except it can be used with a [Navigator] to
+/// show a [Hero] animation.
+class HeroDialogRoute<T> extends PageRoute<T> {
+  HeroDialogRoute({
+    @required this.builder,
+    this.onBackgroundTap,
+  }) : super();
+
+  final WidgetBuilder builder;
+
+  /// Called when the background is tapped.
+  final VoidCallback onBackgroundTap;
+
+  /// The color tween used in the transition to animate the background color.
+  final ColorTween _colorTween = ColorTween(
+    begin: Colors.transparent,
+    end: Colors.black26,
+  );
+
+  @override
+  bool get opaque => false;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  String get barrierLabel => null;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Color get barrierColor => null;
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return Stack(
+      children: <Widget>[
+        GestureDetector(
+          onTap: onBackgroundTap,
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: lerpDouble(0, 3, animation.value),
+              sigmaY: lerpDouble(0, 3, animation.value),
+            ),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: _colorTween.evaluate(animation),
+            ),
+          ),
+        ),
+        FadeTransition(
+          opacity: CurvedAnimation(parent: animation, curve: Curves.easeOut),
+          child: child,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget buildPage(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+  ) {
+    return builder(context);
+  }
+}
