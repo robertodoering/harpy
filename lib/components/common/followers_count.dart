fbcecff1d91beaae40dbbd403a294d248aa5d267
Roberto Doering <rbydoering@gmail.com>
2020-08-03 00:33:35 +0200

    Format followers count numbers

--- lib/components/common/followers_count.dart
+++ lib/components/common/followers_count.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:harpy/components/common/buttons/harpy_button.dart';
 import 'package:harpy/core/api/user_data.dart';
+import 'package:intl/intl.dart';
 
 /// A widget to display the number of following users and followers for the
 /// [user].
 class FollowersCount extends StatelessWidget {
-  const FollowersCount(this.user);
+  FollowersCount(this.user);
 
   final UserData user;
+
+  final NumberFormat _numberFormat = NumberFormat.compact();
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +20,12 @@ class FollowersCount extends StatelessWidget {
       alignment: WrapAlignment.spaceBetween,
       children: <Widget>[
         HarpyButton.flat(
-          text: '${user.friendsCount} Following',
+          text: '${_numberFormat.format(user.friendsCount)} Following',
           padding: EdgeInsets.zero,
           onTap: () {},
         ),
         HarpyButton.flat(
-          text: '${user.followersCount} Followers',
+          text: '${_numberFormat.format(user.followersCount)} Followers',
           padding: EdgeInsets.zero,
           onTap: () {},
         ),
