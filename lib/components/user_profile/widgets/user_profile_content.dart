5c926243b92c576bd181b10999670ace3fd04b5b
Roberto Doering <rbydoering@gmail.com>
2020-08-05 01:35:59 +0200

    Add profile banner dialog image

--- lib/components/user_profile/widgets/user_profile_content.dart
+++ lib/components/user_profile/widgets/user_profile_content.dart
@@ -1,9 +1,15 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:harpy/components/common/custom_dismissible.dart';
 import 'package:harpy/components/common/fading_nested_scaffold.dart';
+import 'package:harpy/components/common/routes/hero_dialog_route.dart';
 import 'package:harpy/components/timeline/user_timeline/widget/user_timeline.dart';
 import 'package:harpy/components/user_profile/bloc/user_profile_bloc.dart';
 import 'package:harpy/components/user_profile/widgets/user_profile_header.dart';
+import 'package:harpy/core/service_locator.dart';
+import 'package:harpy/misc/harpy_navigator.dart';
 
 /// Builds the content for the [UserProfileScreen].
 class UserProfileContent extends StatelessWidget {
@@ -11,14 +17,40 @@ class UserProfileContent extends StatelessWidget {
 
   final UserProfileBloc bloc;
 
+  /// Builds the profile banner for a [HeroDialogRoute] when the user taps on
+  /// the banner.
+  Widget _buildDialogImage() {
+    return CustomDismissible(
+      onDismissed: () => app<HarpyNavigator>().state.maybePop(),
+      child: Center(
+        child: Hero(
+          tag: bloc.user.profileBannerUrl,
+          child: CachedNetworkImage(
+            imageUrl: bloc.user.profileBannerUrl,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _buildAppBarBackground() {
     if (bloc.user.profileBannerUrl != null) {
       return GestureDetector(
-        // todo: open image gallery
-        onTap: () {},
-        child: CachedNetworkImage(
-          imageUrl: bloc.user.profileBannerUrl,
-          fit: BoxFit.cover,
+        onTap: () {
+          app<HarpyNavigator>().pushRoute(HeroDialogRoute<void>(
+            onBackgroundTap: () => app<HarpyNavigator>().state.maybePop(),
+            builder: (BuildContext context) => _buildDialogImage(),
+          ));
+        },
+        child: Hero(
+          tag: bloc.user.profileBannerUrl,
+          placeholderBuilder:
+              (BuildContext context, Size heroSize, Widget child) => child,
+          child: CachedNetworkImage(
+            imageUrl: bloc.user.profileBannerUrl,
+            fit: BoxFit.cover,
+          ),
         ),
       );
     } else {
@@ -28,6 +60,11 @@ class UserProfileContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MediaQueryData mediaQuery = MediaQuery.of(context);
+
+    // limit the app bar extended height
+    final double appBarHeight = min(200, mediaQuery.size.height * .25);
+
     return FadingNestedScaffold(
       title: bloc.user.name,
       alwaysShowTitle: bloc.user.profileBannerUrl == null,
@@ -36,6 +73,7 @@ class UserProfileContent extends StatelessWidget {
         UserProfileHeader(bloc),
       ],
       body: UserTimeline(screenName: bloc.user.screenName),
+      expandedHeight: appBarHeight,
     );
   }
 }
