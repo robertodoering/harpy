e3d64c324f9cb175957dc2b743344a739e1407ef
Roberto Doering <rbydoering@gmail.com>
2020-08-15 22:29:00 +0200

    Move theme selection screen components

---
+++ lib/components/settings/widgets/theme/theme_selection/add_custom_theme_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:harpy/components/common/misc/flare_icons.dart';
+import 'package:harpy/harpy.dart';
+
+/// A card used to add a custom theme for the [ThemeSelectionScreen].
+class AddCustomThemeCard extends StatelessWidget {
+  const AddCustomThemeCard();
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    // only build a trailing icon when using harpy free
+    final Widget trailing = Harpy.isFree
+        ? const FlareIcon.shiningStar(
+            size: 28,
+          )
+        : null;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: theme.dividerColor),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Material(
+        type: MaterialType.transparency,
+        child: ListTile(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          leading: const Icon(Icons.add),
+          title: const Text('Add custom theme'),
+          trailing: trailing,
+          onTap: () {
+            // todo: navigate to custom theme screen
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/components/settings/widgets/theme/theme_selection/theme_selection_screen.dart
+++ lib/components/settings/widgets/theme/theme_selection/theme_selection_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:harpy/components/application/bloc/application_bloc.dart';
 import 'package:harpy/components/application/bloc/application_event.dart';
-import 'package:harpy/components/common/misc/flare_icons.dart';
 import 'package:harpy/components/common/misc/harpy_scaffold.dart';
+import 'package:harpy/components/settings/widgets/theme/theme_selection/add_custom_theme_card.dart';
+import 'package:harpy/components/settings/widgets/theme/theme_selection/theme_card.dart';
 import 'package:harpy/core/theme/harpy_theme.dart';
 import 'package:harpy/core/theme/predefined_themes.dart';
-import 'package:harpy/harpy.dart';
 
 class ThemeSelectionScreen extends StatefulWidget {
   const ThemeSelectionScreen();
@@ -30,99 +30,19 @@ class _ThemeSelectionScreenState extends State<ThemeSelectionScreen> {
             ThemeCard(
               harpyTheme,
               selected: harpyTheme == activeHarpyTheme,
-              onTap: () => setState(() {
-                applicationBloc.add(ChangeThemeEvent(
-                  harpyTheme: harpyTheme,
-                  id: predefinedThemes.indexOf(harpyTheme),
-                ));
-              }),
+              onTap: () {
+                if (harpyTheme != activeHarpyTheme) {
+                  setState(() {
+                    applicationBloc.add(ChangeThemeEvent(
+                      harpyTheme: harpyTheme,
+                      id: predefinedThemes.indexOf(harpyTheme),
+                    ));
+                  });
+                }
+              },
             ),
           const AddCustomThemeCard(),
         ],
-      ),
-    );
-  }
-}
-
-class ThemeCard extends StatelessWidget {
-  const ThemeCard(
-    this.harpyTheme, {
-    this.selected = false,
-    this.onTap,
-  });
-
-  final HarpyTheme harpyTheme;
-
-  final bool selected;
-
-  final VoidCallback onTap;
-
-  LinearGradient get gradient => LinearGradient(
-      colors: harpyTheme.backgroundColors.length > 1
-          ? harpyTheme.backgroundColors
-          : <Color>[
-              harpyTheme.backgroundColors.first,
-              harpyTheme.backgroundColors.first
-            ]);
-
-  @override
-  Widget build(BuildContext context) {
-    return Theme(
-      data: harpyTheme.data,
-      child: Card(
-        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        color: Colors.transparent,
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(gradient: gradient),
-          child: Material(
-            type: MaterialType.transparency,
-            child: ListTile(
-              title: Text(
-                harpyTheme.name,
-              ),
-              trailing: selected ? const Icon(Icons.check) : null,
-              onTap: onTap,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class AddCustomThemeCard extends StatelessWidget {
-  const AddCustomThemeCard();
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-
-    // only build a trailing icon when using harpy free
-    final Widget trailing = Harpy.isFree
-        ? const FlareIcon.shiningStar(
-            size: 28,
-          )
-        : null;
-
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: theme.dividerColor),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Material(
-        type: MaterialType.transparency,
-        child: ListTile(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          leading: const Icon(Icons.add),
-          title: const Text('Add custom theme'),
-          trailing: trailing,
-          onTap: () {},
-        ),
       ),
     );
   }
