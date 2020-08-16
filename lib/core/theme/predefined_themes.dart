b63ca18d1a335a1ee860e6ba73e6ef32b1a20223
Roberto Doering <rbydoering@gmail.com>
2020-08-16 13:27:33 +0200

    Update predefined themes

--- lib/core/theme/predefined_themes.dart
+++ lib/core/theme/predefined_themes.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:harpy/core/theme/harpy_theme.dart';
 import 'package:harpy/core/theme/harpy_theme_data.dart';
 
@@ -12,7 +11,7 @@ final List<HarpyTheme> predefinedThemes = <HarpyTheme>[
 
 final HarpyThemeData crow = HarpyThemeData()
   ..name = 'crow'
-  ..backgroundColors = <int>[0xff000000, 0xff17233d]
+  ..backgroundColors = <int>[0xff000005, 0xff17233d]
   ..accentColor = 0xff4178f0;
 
 final HarpyThemeData swan = HarpyThemeData()
@@ -22,10 +21,10 @@ final HarpyThemeData swan = HarpyThemeData()
 
 final HarpyThemeData phoenix = HarpyThemeData()
   ..name = 'phoenix'
-  ..backgroundColors = <int>[0xff9e0000, 0xffd1670a]
-  ..accentColor = Colors.orangeAccent.value;
+  ..backgroundColors = <int>[0xfffd1320, 0xffff6215]
+  ..accentColor = 0xffffD600;
 
 final HarpyThemeData harpy = HarpyThemeData()
   ..name = 'harpy'
-  ..backgroundColors = <int>[0xff40148b, 0xff5b1051, 0xff850a2f]
-  ..accentColor = 0xffd4d4d4;
+  ..backgroundColors = <int>[0xff40148b, 0xff850a2f]
+  ..accentColor = 0xffEB3965;
