52ea33c6de7f63d58976db8b9f0fb1d9e670cba9
Roberto Doering <rbydoering@gmail.com>
2020-08-23 21:35:34 +0200

    Build custom themes in theme selection

--- lib/components/settings/widgets/theme/theme_selection/theme_selection_screen.dart
+++ lib/components/settings/widgets/theme/theme_selection/theme_selection_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:harpy/components/application/bloc/application_bloc.dart';
 import 'package:harpy/components/application/bloc/application_event.dart';
 import 'package:harpy/components/common/misc/harpy_scaffold.dart';
+import 'package:harpy/components/settings/bloc/custom_theme/custom_theme_bloc.dart';
 import 'package:harpy/components/settings/widgets/theme/theme_selection/add_custom_theme_card.dart';
 import 'package:harpy/components/settings/widgets/theme/theme_selection/theme_card.dart';
+import 'package:harpy/core/preferences/theme_preferences.dart';
+import 'package:harpy/core/service_locator.dart';
 import 'package:harpy/core/theme/harpy_theme.dart';
 import 'package:harpy/core/theme/predefined_themes.dart';
 
@@ -17,31 +20,69 @@ class ThemeSelectionScreen extends StatefulWidget {
 }
 
 class _ThemeSelectionScreenState extends State<ThemeSelectionScreen> {
+  void _changeTheme(
+    ApplicationBloc applicationBloc,
+    int selectedThemeId,
+    int newThemeId,
+  ) {
+    if (selectedThemeId != newThemeId) {
+      setState(() {
+        applicationBloc.add(ChangeThemeEvent(
+          id: newThemeId,
+          saveSelection: true,
+        ));
+      });
+    }
+  }
+
+  List<Widget> _buildPredefinedThemes(
+    ApplicationBloc applicationBloc,
+    int selectedThemeId,
+  ) {
+    return <Widget>[
+      for (int i = 0; i < predefinedThemes.length; i++)
+        ThemeCard(
+          predefinedThemes[i],
+          selected: i == selectedThemeId,
+          onTap: () => _changeTheme(applicationBloc, selectedThemeId, i),
+        ),
+    ];
+  }
+
+  List<Widget> _buildCustomThemes(
+    ApplicationBloc applicationBloc,
+    int selectedThemeId,
+    List<HarpyTheme> customThemes,
+  ) {
+    return <Widget>[
+      for (int i = 0; i < customThemes.length; i++)
+        ThemeCard(
+          customThemes[i],
+          selected: i + 10 == selectedThemeId,
+          onTap: () => _changeTheme(applicationBloc, selectedThemeId, i + 10),
+          // todo: delete action
+          // todo: edit action
+        ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     final ApplicationBloc applicationBloc = ApplicationBloc.of(context);
-    final HarpyTheme activeHarpyTheme = HarpyTheme.of(context);
+    final CustomThemeBloc customThemeBloc = applicationBloc.customThemeBloc;
+    final int selectedThemeId = app<ThemePreferences>().selectedTheme;
 
     return HarpyScaffold(
       title: 'Theme selection',
       body: Column(
         children: <Widget>[
-          for (HarpyTheme harpyTheme in predefinedThemes)
-            ThemeCard(
-              harpyTheme,
-              selected: harpyTheme == activeHarpyTheme,
-              onTap: () {
-                if (harpyTheme != activeHarpyTheme) {
-                  setState(() {
-                    applicationBloc.add(ChangeThemeEvent(
-                      id: predefinedThemes.indexOf(harpyTheme),
-                      saveSelection: true,
-                    ));
-                  });
-                }
-              },
-            ),
+          ..._buildPredefinedThemes(applicationBloc, selectedThemeId),
           const AddCustomThemeCard(),
+          ..._buildCustomThemes(
+            applicationBloc,
+            selectedThemeId,
+            customThemeBloc.customThemes,
+          ),
         ],
       ),
     );
