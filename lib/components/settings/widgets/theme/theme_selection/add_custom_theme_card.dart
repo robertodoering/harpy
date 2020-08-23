7e9cbde8c198c1346e1cf1ba6ef0870e138e470b
Roberto Doering <rbydoering@gmail.com>
2020-08-23 21:35:53 +0200

    Navigate to custom theme screen in pro

--- lib/components/settings/widgets/theme/theme_selection/add_custom_theme_card.dart
+++ lib/components/settings/widgets/theme/theme_selection/add_custom_theme_card.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:harpy/components/common/dialogs/pro_dialog.dart';
 import 'package:harpy/components/common/misc/flare_icons.dart';
+import 'package:harpy/components/settings/bloc/custom_theme/custom_theme_bloc.dart';
 import 'package:harpy/core/message_service.dart';
 import 'package:harpy/core/service_locator.dart';
+import 'package:harpy/core/theme/harpy_theme.dart';
+import 'package:harpy/core/theme/harpy_theme_data.dart';
 import 'package:harpy/harpy.dart';
+import 'package:harpy/misc/harpy_navigator.dart';
 
 /// A card used to add a custom theme for the [ThemeSelectionScreen].
 class AddCustomThemeCard extends StatelessWidget {
   const AddCustomThemeCard();
 
-  Future<void> _onTap(BuildContext context) async {
+  Future<void> _onTap(
+    BuildContext context,
+    HarpyThemeData themeData,
+    int themeId,
+  ) async {
     if (Harpy.isFree) {
       final bool result = await showDialog<bool>(
         context: context,
@@ -23,14 +31,24 @@ class AddCustomThemeCard extends StatelessWidget {
         app<MessageService>().showInfo('Not yet available');
       }
     } else {
-      // todo: navigate to custom theme screen
-      app<MessageService>().showInfo('Not yet available');
+      app<HarpyNavigator>().pushCustomTheme(
+        themeData: themeData,
+        themeId: themeId,
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    final CustomThemeBloc customThemeBloc = CustomThemeBloc.of(context);
+
+    // the initial custom theme data uses the currently selected theme
+    final HarpyThemeData initialCustomThemeData =
+        HarpyThemeData.fromHarpyTheme(HarpyTheme.of(context));
+
+    // use the next available custom theme id
+    final int nextCustomThemeId = customThemeBloc.customThemes.length + 10;
 
     // only build a trailing icon when using harpy free
     final Widget trailing =
@@ -50,7 +68,11 @@ class AddCustomThemeCard extends StatelessWidget {
           leading: const Icon(Icons.add),
           title: const Text('Add custom theme'),
           trailing: trailing,
-          onTap: () => _onTap(context),
+          onTap: () => _onTap(
+            context,
+            initialCustomThemeData,
+            nextCustomThemeId,
+          ),
         ),
       ),
     );
