ac07afebb61c625fbbf55d84fca7cd6d6d0b6672
Roberto Doering <rbydoering@gmail.com>
2020-08-29 22:53:37 +0200

    Move custom theme background colors

--- lib/components/settings/custom_theme/widgets/custom_theme_screen.dart
+++ lib/components/settings/custom_theme/widgets/custom_theme_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:harpy/components/common/dialogs/color_picker_dialog.dart';
 import 'package:harpy/components/common/misc/harpy_scaffold.dart';
 import 'package:harpy/components/settings/custom_theme/bloc/custom_theme_bloc.dart';
 import 'package:harpy/components/settings/custom_theme/bloc/custom_theme_state.dart';
+import 'package:harpy/components/settings/custom_theme/widgets/content/background_colors.dart';
 import 'package:harpy/core/theme/harpy_theme.dart';
 import 'package:harpy/core/theme/harpy_theme_data.dart';
 
@@ -64,91 +64,6 @@ class CustomThemeScreen extends StatelessWidget {
           );
         },
       ),
-    );
-  }
-}
-
-class CustomThemeBackgroundColors extends StatelessWidget {
-  const CustomThemeBackgroundColors(this.bloc);
-
-  final CustomThemeBloc bloc;
-
-  String _nameFromIndex(int index) {
-    // todo: move to string utils
-    if (index == 0) {
-      return 'First';
-    } else if (index == 1) {
-      return 'Second';
-    } else if (index == 2) {
-      return 'Third';
-    } else if (index == 3) {
-      return 'Fourth';
-    } else {
-      return 'Fifth';
-    }
-  }
-
-  List<Widget> _buildBackgroundColors(HarpyTheme harpyTheme) {
-    return <Widget>[
-      for (int i = 0; i < harpyTheme.backgroundColors.length; i++)
-        Card(
-          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          color: harpyTheme.backgroundColors[i],
-          clipBehavior: Clip.antiAlias,
-          child: Container(
-            width: double.infinity,
-            child: Material(
-              type: MaterialType.transparency,
-              child: ListTile(
-                title: Text('${_nameFromIndex(i)} background color'),
-                // todo: change color
-                onTap: () {},
-              ),
-            ),
-          ),
-        )
-    ];
-  }
-
-  Widget _buildAddBackgroundColor(BuildContext context, HarpyTheme harpyTheme) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: harpyTheme.data.dividerColor),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Material(
-        type: MaterialType.transparency,
-        child: ListTile(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          leading: const Icon(Icons.add),
-          title: const Text('Add background color'),
-          onTap: () async {
-            final Color color = await showDialog<Color>(
-              context: context,
-              builder: (BuildContext context) => ColorPickerDialog(
-                color: harpyTheme.backgroundColors.first,
-              ),
-            );
-
-            // todo: add background color
-          },
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final HarpyTheme harpyTheme = bloc.harpyTheme;
-
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        ..._buildBackgroundColors(harpyTheme),
-        _buildAddBackgroundColor(context, harpyTheme),
-      ],
     );
   }
 }
