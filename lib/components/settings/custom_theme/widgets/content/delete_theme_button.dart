27708da8358b6e798e0741748b31a61318577e5e
Roberto Doering <rbydoering@gmail.com>
2020-09-05 22:42:47 +0200

    Add delete theme button

---
+++ lib/components/settings/custom_theme/widgets/content/delete_theme_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:harpy/components/common/buttons/harpy_button.dart';
+import 'package:harpy/components/settings/custom_theme/bloc/custom_theme_bloc.dart';
+import 'package:harpy/components/settings/custom_theme/bloc/custom_theme_event.dart';
+
+/// Builds a button to delete a custom theme for the [CustomThemeScreen].
+class DeleteThemeButton extends StatelessWidget {
+  const DeleteThemeButton(this.bloc);
+
+  final CustomThemeBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: HarpyButton.raised(
+        text: 'Delete theme',
+        onTap: () => bloc.add(const DeleteCustomTheme()),
+      ),
+    );
+  }
+}
