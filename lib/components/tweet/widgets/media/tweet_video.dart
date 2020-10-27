ec08ddb93446cfdba1a7b1ff675de24db046147c
Roberto Doering <rbydoering@gmail.com>
2020-10-27 11:10:46 +0100

    Add custom dismissible for video media overlay

--- lib/components/tweet/widgets/media/tweet_video.dart
+++ lib/components/tweet/widgets/media/tweet_video.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:harpy/components/common/misc/custom_dismissible.dart';
 import 'package:harpy/components/common/video_player/harpy_video_player.dart';
 import 'package:harpy/components/common/video_player/harpy_video_player_model.dart';
 import 'package:harpy/components/tweet/bloc/tweet_bloc.dart';
 import 'package:harpy/components/tweet/widgets/overlay/media_overlay.dart';
 import 'package:harpy/core/api/twitter/tweet_data.dart';
+import 'package:harpy/core/service_locator.dart';
 import 'package:harpy/core/theme/harpy_theme.dart';
+import 'package:harpy/misc/harpy_navigator.dart';
 
 /// Builds a [HarpyVideoPlayer] for the [TweetMedia] video.
 class TweetVideo extends StatelessWidget {
@@ -21,12 +24,15 @@ class TweetVideo extends StatelessWidget {
       tweet: tweet,
       tweetBloc: tweetBloc,
       enableImmersiveMode: false,
-      child: HarpyVideoPlayer.fromModel(
-        model,
-        thumbnail: tweet.video.thumbnailUrl,
-        thumbnailAspectRatio: tweet.video.validAspectRatio
-            ? tweet.video.aspectRatioDouble
-            : 16 / 9,
+      child: CustomDismissible(
+        onDismissed: () => app<HarpyNavigator>().state.maybePop(),
+        child: HarpyVideoPlayer.fromModel(
+          model,
+          thumbnail: tweet.video.thumbnailUrl,
+          thumbnailAspectRatio: tweet.video.validAspectRatio
+              ? tweet.video.aspectRatioDouble
+              : 16 / 9,
+        ),
       ),
     );
   }
