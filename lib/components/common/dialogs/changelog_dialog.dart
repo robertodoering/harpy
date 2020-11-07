fc2066f3f07a293f6b43247bc98043410f17e9aa
Roberto Doering <rbydoering@gmail.com>
2020-11-07 04:04:26 +0100

    Implemented changelog dialog

--- lib/components/common/dialogs/changelog_dialog.dart
+++ lib/components/common/dialogs/changelog_dialog.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:harpy/components/common/dialogs/harpy_dialog.dart';
 import 'package:harpy/core/harpy_info.dart';
 import 'package:harpy/core/preferences/changelog_preferences.dart';
 import 'package:harpy/core/service_locator.dart';
+import 'package:harpy/harpy.dart';
+import 'package:harpy/misc/changelog_parser.dart';
+import 'package:harpy/misc/harpy_navigator.dart';
 
 class ChangelogDialog extends StatefulWidget {
   const ChangelogDialog();
@@ -30,29 +32,156 @@ class ChangelogDialog extends StatefulWidget {
 }
 
 class _ChangelogDialogState extends State<ChangelogDialog> {
-  String _text = '';
+  Future<ChangelogData> _changelogData;
+
+  final HarpyInfo harpyInfo = app<HarpyInfo>();
 
   @override
   void initState() {
     super.initState();
 
-    rootBundle
-        .loadString(
-          'android/fastlane/metadata/android/en-US/changelogs/17.txt',
-          cache: false,
-        )
-        .then(
-          (String value) => setState(() {
-            _text = value;
-          }),
-        );
+    _changelogData = app<ChangelogParser>().current();
+  }
+
+  Widget _spacedColumn(List<Widget> children) {
+    return Column(
+      children: <Widget>[
+        for (Widget child in children) ...<Widget>[
+          child,
+          if (child != children.last) const SizedBox(height: 12),
+        ],
+      ],
+    );
+  }
+
+  Widget _buildVersionText() {
+    final String flavor = Harpy.isFree ? 'Free' : 'Pro';
+    final String version = harpyInfo.packageInfo.version;
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Text('Harpy $flavor $version'),
+    );
+  }
+
+  Widget _buildChangelogData(ChangelogData data) {
+    final List<Widget> additions = data.additions
+        .map((ChangelogEntry entry) =>
+            _buildEntry(entry, const Icon(Icons.add, color: Colors.green)))
+        .toList();
+
+    final List<Widget> changes = data.changes
+        .map((ChangelogEntry entry) => _buildEntry(
+            entry, const Icon(Icons.compare_arrows, color: Colors.yellow)))
+        .toList();
+
+    final List<Widget> fixes = data.fixes
+        .map((ChangelogEntry entry) => _buildEntry(
+            entry, const Icon(Icons.bug_report_outlined, color: Colors.green)))
+        .toList();
+
+    final List<Widget> removals = data.removals
+        .map((ChangelogEntry entry) =>
+            _buildEntry(entry, const Icon(Icons.remove, color: Colors.red)))
+        .toList();
+
+    final List<Widget> others = data.others
+        .map((ChangelogEntry entry) => _buildEntry(
+            entry, const Icon(Icons.info_outline, color: Colors.yellow)))
+        .toList();
+
+    return _spacedColumn(<Widget>[
+      _buildVersionText(),
+      if (additions.isNotEmpty) _spacedColumn(additions),
+      if (changes.isNotEmpty) _spacedColumn(changes),
+      if (fixes.isNotEmpty) _spacedColumn(fixes),
+      if (removals.isNotEmpty) _spacedColumn(removals),
+      if (others.isNotEmpty) _spacedColumn(others),
+    ]);
+  }
+
+  Widget _buildEntry(ChangelogEntry entry, Widget icon) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        icon,
+        const SizedBox(width: 24),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(entry.line, textAlign: TextAlign.left),
+              if (entry.additionalInfo.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(top: 6, left: 12),
+                  child: _buildAdditionalInformation(entry.additionalInfo),
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAdditionalInformation(List<String> additionalInformation) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        for (String line in additionalInformation) ...<Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text('·'),
+              const SizedBox(width: 12),
+              Expanded(child: Text('$line', textAlign: TextAlign.left)),
+            ],
+          ),
+          if (line != additionalInformation.last) const SizedBox(height: 6),
+        ]
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return HarpyDialog(
       title: const Text('Whats new?'),
-      content: Text(_text),
+      contentPadding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+      content: FutureBuilder<ChangelogData>(
+        future: _changelogData,
+        builder: (BuildContext context, AsyncSnapshot<ChangelogData> snapshot) {
+          Widget child;
+
+          if (snapshot.hasData) {
+            child = _buildChangelogData(snapshot.data);
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            child = const CircularProgressIndicator();
+          } else {
+            child = const Text('No changelog found');
+          }
+
+          return Column(
+            children: <Widget>[
+              child,
+              const SizedBox(height: 24),
+              CheckboxListTile(
+                value: false,
+                title: Text(
+                  "Don't show this again",
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+                onChanged: (bool value) {},
+              ),
+            ],
+          );
+        },
+      ),
+      actions: <Widget>[
+        DialogAction<void>(
+          text: 'Ok',
+          onTap: () => app<HarpyNavigator>().state.maybePop(),
+        )
+      ],
     );
   }
 }
