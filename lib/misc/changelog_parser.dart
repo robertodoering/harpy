1672d84b282345c658800d33f1870e695c99d67a
Roberto Doering <rbydoering@gmail.com>
2020-11-07 02:11:10 +0100

    Implement changelog parser & tests

---
+++ lib/misc/changelog_parser.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:harpy/core/harpy_info.dart';
+import 'package:harpy/core/service_locator.dart';
+import 'package:harpy/harpy.dart';
+
+/// Parses the [ChangelogData] from a changelog file located in
+/// `android/fastlane/metadata/android/$flavor/en-US/changelogs/$versionCode.txt`.
+class ChangelogParser {
+  static const String linePrefix = '· ';
+
+  /// Returns the [ChangelogData] for the current version.
+  ///
+  /// Returns `null` if the current version has no changelog file.
+  Future<ChangelogData> current() async {
+    return parse(app<HarpyInfo>().packageInfo.buildNumber);
+  }
+
+  /// Returns the [ChangelogData] for the [versionCode].
+  ///
+  /// Returns `null` if no corresponding changelog file exists.
+  Future<ChangelogData> parse(String versionCode) async {
+    try {
+      final String changelogString = await rootBundle.loadString(
+        _changelogString(versionCode),
+        cache: false,
+      );
+
+      final ChangelogData data = ChangelogData();
+
+      ChangelogEntry entry;
+
+      for (String line in changelogString.split('\n')) {
+        line = line.replaceFirst(linePrefix, '');
+
+        if (line.startsWith(' ') && entry != null) {
+          // additional information for the last line
+          entry?.additionalInfo?.add(line.trim());
+        } else if (line.startsWith('Added')) {
+          entry = _parseLine(line, 'Added');
+          data.additions.add(entry);
+        } else if (line.startsWith('Changed')) {
+          entry = _parseLine(line, 'Changed');
+          data.changes.add(entry);
+        } else if (line.startsWith('Fixed')) {
+          entry = _parseLine(line, 'Fixed');
+          data.fixes.add(entry);
+        } else if (line.startsWith('Removed')) {
+          entry = _parseLine(line, 'Removed');
+          data.removals.add(entry);
+        } else {
+          entry = _parseLine(line);
+          data.others.add(entry);
+        }
+      }
+
+      return data;
+    } catch (e) {
+      // changelog file not found
+      return null;
+    }
+  }
+
+  String _changelogString(String versionCode) {
+    final String flavor = Harpy.isFree ? 'free' : 'pro';
+
+    return 'android/fastlane/metadata/android'
+        '/$flavor/en-US/changelogs/$versionCode.txt';
+  }
+
+  ChangelogEntry _parseLine(String line, [String prefix = '']) {
+    return ChangelogEntry(line: line.replaceFirst(prefix, '').trim());
+  }
+}
+
+/// Represents the data of a changelog for one version.
+class ChangelogData {
+  /// Entries that start with 'Added'.
+  final List<ChangelogEntry> additions = <ChangelogEntry>[];
+
+  /// Entries that start with 'Changed'.
+  final List<ChangelogEntry> changes = <ChangelogEntry>[];
+
+  /// Entries that start with 'Fixed'.
+  final List<ChangelogEntry> fixes = <ChangelogEntry>[];
+
+  /// Entries that start with 'Removed'.
+  final List<ChangelogEntry> removals = <ChangelogEntry>[];
+
+  /// Entries that don't have a predefined prefix.
+  final List<ChangelogEntry> others = <ChangelogEntry>[];
+
+  /// Whether no [ChangelogEntry] exists.
+  bool get empty =>
+      additions.isEmpty && changes.isEmpty && fixes.isEmpty && removals.isEmpty;
+}
+
+/// Represents a single entry for a changelog.
+///
+/// Most entries consist of a single [line] but can have multiple additional
+/// lines in [additionalInfo].
+class ChangelogEntry {
+  ChangelogEntry({
+    @required this.line,
+  });
+
+  final String line;
+  final List<String> additionalInfo = <String>[];
+}
+
+enum ChangelogType {
+  addition,
+  change,
+  fix,
+  removal,
+  other,
+}
---
+++ test/unit_tests/misc/changelog_parser_test.dart
@@ -0,0 +1,120 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:harpy/core/harpy_info.dart';
+import 'package:harpy/core/service_locator.dart';
+import 'package:harpy/misc/changelog_parser.dart';
+import 'package:mockito/mockito.dart';
+import 'package:package_info/package_info.dart';
+
+class MockHarpyInfo extends Mock implements HarpyInfo {}
+
+void main() {
+  setUp(() {
+    app.registerLazySingleton<HarpyInfo>(() => MockHarpyInfo());
+    app.registerLazySingleton<ChangelogParser>(() => ChangelogParser());
+
+    when(app<HarpyInfo>().packageInfo).thenReturn(
+      PackageInfo(buildNumber: '14'),
+    );
+  });
+
+  tearDown(app.reset);
+
+  group('changelog parser', () {
+    test('parses a changelog file with single line entries', () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      ServicesBinding.instance.defaultBinaryMessenger.setMockMessageHandler(
+        'flutter/assets',
+        (ByteData message) async =>
+            utf8.encoder.convert(_changelog1).buffer.asByteData(),
+      );
+
+      final ChangelogData data = await app<ChangelogParser>().current();
+
+      expect(data.empty, isFalse);
+      expect(data.additions.length, equals(3), reason: 'invalid additions');
+      expect(data.changes.length, equals(1), reason: 'invalid changes');
+      expect(data.fixes.length, equals(1), reason: 'invalid fixes');
+      expect(data.removals.length, equals(1), reason: 'invalid removals');
+      expect(data.others.length, equals(1), reason: 'invalid others');
+    });
+
+    test('removes prefix from single line entries', () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      ServicesBinding.instance.defaultBinaryMessenger.setMockMessageHandler(
+        'flutter/assets',
+        (ByteData message) async =>
+            utf8.encoder.convert(_changelog1).buffer.asByteData(),
+      );
+
+      final ChangelogData data = await app<ChangelogParser>().current();
+
+      expect(data.additions.first.line, equals('about screen'));
+      expect(data.changes.first.line, equals('the design for info messages'));
+      expect(
+        data.fixes.first.line,
+        equals('video player central replay button not working'),
+      );
+      expect(data.removals.first.line, equals('my sanity'));
+      expect(data.others.first.line, equals('Unrelated changelog entry'));
+    });
+
+    test('parses a changelog file with additional info entries', () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      ServicesBinding.instance.defaultBinaryMessenger.setMockMessageHandler(
+        'flutter/assets',
+        (ByteData message) async =>
+            utf8.encoder.convert(_changelog2).buffer.asByteData(),
+      );
+
+      final ChangelogData data = await app<ChangelogParser>().current();
+
+      expect(data.additions.length, equals(2));
+      expect(data.additions.first.line, equals('Twitter Media settings:'));
+      expect(data.additions.first.additionalInfo.length, equals(3));
+      expect(
+        data.additions.first.additionalInfo.first,
+        equals('Set the quality when using WiFi'),
+      );
+    });
+
+    test(
+        'returns an empty changelog data object '
+        'if changelog file is empty', () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      ServicesBinding.instance.defaultBinaryMessenger.setMockMessageHandler(
+        'flutter/assets',
+        (ByteData message) async =>
+            utf8.encoder.convert(_changelog3).buffer.asByteData(),
+      );
+
+      final ChangelogData data = await app<ChangelogParser>().current();
+
+      expect(data.empty, isTrue);
+    });
+  });
+}
+
+const String _changelog1 = '''· Added about screen
+· Added theme selection settings with 4 predefined themes
+· Added setup screen for the first login
+· Changed the design for info messages
+· Fixed video player central replay button not working
+· Removed my sanity
+· Unrelated changelog entry''';
+
+const String _changelog2 = '''· Added Twitter Media settings:
+    · Set the quality when using WiFi
+    · Set the quality when using mobile data
+    · Autoplay gifs
+· Added launching urls when tapping a url
+· Fixed animation when viewing user profiles
+· Fixed Tweet animation abruptly stopping when changing scroll direction
+· Removed my sanity
+· Unrelated changelog entry
+''';
+
+const String _changelog3 = '''
+''';
