10604ca2202d0bd59e39b8806549c572ba5cca42
Roberto Doering <rbydoering@gmail.com>
2020-11-17 09:50:37 +0100

    Add user search bloc

---
+++ lib/components/search/user/bloc/user_search_event.dart
@@ -0,0 +1,43 @@
+import 'package:dart_twitter_api/api/users/data/user.dart';
+import 'package:harpy/components/common/paginated_bloc/paginated_bloc.dart';
+import 'package:harpy/components/common/paginated_bloc/paginated_event.dart';
+import 'package:harpy/components/search/user/bloc/user_search_bloc.dart';
+import 'package:harpy/core/api/network_error_handler.dart';
+import 'package:harpy/core/api/twitter/user_data.dart';
+import 'package:logging/logging.dart';
+
+class SearchUsers extends LoadPaginatedData {
+  const SearchUsers(this.query);
+
+  final String query;
+
+  static final Logger _log = Logger('SearchUsers');
+
+  List<UserData> _transformResponse(List<User> users) {
+    return users.map((User user) => UserData.fromUser(user)).toList();
+  }
+
+  @override
+  Future<bool> loadData(PaginatedBloc paginatedBloc) async {
+    final UserSearchBloc bloc = paginatedBloc as UserSearchBloc;
+    bloc.lastQuery = query;
+
+    final List<UserData> users = await bloc.userService
+        .usersSearch(
+          q: query,
+          count: 20,
+          page: bloc.cursor,
+          includeEntities: false,
+        )
+        .then(_transformResponse)
+        .catchError(silentErrorHandler);
+
+    if (users != null) {
+      _log.fine('found ${users.length} users');
+      bloc.users = users;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
