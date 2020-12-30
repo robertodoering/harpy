8ea3d73e11c192c86633a201454cdefd07f2d874
Roberto Doering <rbydoering@gmail.com>
2020-12-30 22:31:05 +0100

    Implement tweet actions

--- lib/components/tweet/widgets/tweet/content/actions_button.dart
+++ lib/components/tweet/widgets/tweet/content/actions_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:harpy/components/common/buttons/view_more_action_button.dart';
 import 'package:harpy/components/tweet/bloc/tweet_bloc.dart';
+import 'package:harpy/components/tweet/bloc/tweet_event.dart';
 import 'package:harpy/core/api/twitter/tweet_data.dart';
 import 'package:harpy/core/service_locator.dart';
 import 'package:harpy/misc/harpy_navigator.dart';
@@ -25,6 +26,7 @@ class TweetActionsButton extends StatelessWidget {
           leading: const Icon(Icons.open_in_browser),
           title: const Text('Open externally'),
           onTap: () {
+            bloc.add(OpenTweetExternally(tweet: tweet));
             app<HarpyNavigator>().state.maybePop();
           },
         ),
@@ -33,6 +35,7 @@ class TweetActionsButton extends StatelessWidget {
           title: const Text('Copy text'),
           enabled: bloc.tweet.hasText,
           onTap: () {
+            bloc.add(CopyTweetText(tweet: tweet));
             app<HarpyNavigator>().state.maybePop();
           },
         ),
@@ -40,6 +43,7 @@ class TweetActionsButton extends StatelessWidget {
           leading: const Icon(Icons.share),
           title: const Text('Share Tweet'),
           onTap: () {
+            bloc.add(ShareTweet(tweet: tweet));
             app<HarpyNavigator>().state.maybePop();
           },
         ),
