6f5d7f2973c64b0883dfb0d594ebea5fcbf43cc5
Roberto Doering <rbydoering@gmail.com>
2020-12-30 02:35:17 +0100

    Add view more action button

---
+++ lib/components/common/buttons/view_more_action_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:harpy/components/common/buttons/harpy_button.dart';
+import 'package:harpy/components/settings/layout/widgets/layout_padding.dart';
+import 'package:harpy/core/theme/harpy_theme.dart';
+
+/// Builds a button that opens a modal bottom sheet with the [children] in a
+/// column.
+class ViewMoreActionButton extends StatelessWidget {
+  const ViewMoreActionButton({
+    @required this.children,
+  });
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    final HarpyTheme harpyTheme = HarpyTheme.of(context);
+
+    return HarpyButton.flat(
+      icon: const Icon(Icons.more_vert),
+      padding: const EdgeInsets.all(16),
+      onTap: () {
+        showModalBottomSheet<void>(
+          context: context,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: kDefaultRadius,
+              topRight: kDefaultRadius,
+            ),
+          ),
+          builder: (BuildContext context) => Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // handle
+              Container(
+                width: 50,
+                height: 2,
+                margin: EdgeInsets.all(defaultPaddingValue / 4),
+                decoration: BoxDecoration(
+                  borderRadius: kDefaultBorderRadius,
+                  color: harpyTheme.foregroundColor.withOpacity(.2),
+                ),
+              ),
+              defaultSmallVerticalSpacer,
+              ...children,
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
