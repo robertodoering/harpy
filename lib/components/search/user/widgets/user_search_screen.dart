4c5192a06c65f82a8ac375643722a9e0870b3fa7
Roberto Doering <rbydoering@gmail.com>
2021-01-17 10:52:38 +0100

    Implement user search screen

--- lib/components/search/user/widgets/user_search_screen.dart
+++ lib/components/search/user/widgets/user_search_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:harpy/components/common/list/scroll_direction_listener.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harpy/components/common/misc/harpy_scaffold.dart';
-import 'package:harpy/components/common/misc/harpy_sliver_app_bar.dart';
-import 'package:harpy/components/search/widgets/search_text_field.dart';
-import 'package:harpy/components/settings/layout/widgets/layout_padding.dart';
-import 'package:harpy/components/user/widgets/user_list.dart';
-import 'package:harpy/core/api/twitter/user_data.dart';
+import 'package:harpy/components/search/user/bloc/user_search_bloc.dart';
+import 'package:harpy/components/search/user/widgets/content/user_search_list.dart';
 
 class UserSearchScreen extends StatelessWidget {
   const UserSearchScreen();
@@ -14,31 +11,10 @@ class UserSearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MediaQueryData mediaQuery = MediaQuery.of(context);
-
     return HarpyScaffold(
-      body: ScrollDirectionListener(
-        child: UserList(
-          <UserData>[],
-          beginSlivers: <Widget>[
-            HarpySliverAppBar(
-              title: 'Search users',
-              floating: true,
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(60),
-                child: Padding(
-                  padding: DefaultEdgeInsets.all(),
-                  child: const SearchTextField(),
-                ),
-              ),
-            ),
-          ],
-          endSlivers: <Widget>[
-            SliverToBoxAdapter(
-              child: SizedBox(height: mediaQuery.padding.bottom),
-            )
-          ],
-        ),
+      body: BlocProvider<UserSearchBloc>(
+        create: (_) => UserSearchBloc(),
+        child: const UserSearchList(),
       ),
     );
   }
