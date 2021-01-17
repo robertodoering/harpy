0ee47c7d71e8b0e82749f72cda22b1457b986fa2
Roberto Doering <rbydoering@gmail.com>
2021-01-17 09:58:51 +0100

    Implement search text field

--- lib/components/search/widgets/search_text_field.dart
+++ lib/components/search/widgets/search_text_field.dart
@@ -2,10 +2,84 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:harpy/components/common/animations/animation_constants.dart';
 import 'package:harpy/core/theme/harpy_theme.dart';
 
-class SearchTextField extends StatelessWidget {
-  const SearchTextField();
+class SearchTextField extends StatefulWidget {
+  const SearchTextField({
+    this.onSubmitted,
+    this.onClear,
+    this.requestFocus = false,
+  });
+
+  final ValueChanged<String> onSubmitted;
+  final VoidCallback onClear;
+  final bool requestFocus;
+
+  @override
+  _SearchTextFieldState createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  TextEditingController _controller;
+  FocusNode _focusNode;
+
+  bool _showClear = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = TextEditingController()
+      ..addListener(() {
+        if (_showClear != _controller.value.text.isNotEmpty) {
+          setState(() => _showClear = !_showClear);
+        }
+      });
+
+    _focusNode = FocusNode();
+
+    if (widget.requestFocus) {
+      _focusNode.requestFocus();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    _focusNode.dispose();
+  }
+
+  Widget _buildClearIcon(ThemeData theme) {
+    Widget child;
+
+    if (_showClear) {
+      child = GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          _controller.clear();
+          widget.onClear();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Icon(
+            Icons.close,
+            color: theme.iconTheme.color.withOpacity(.7),
+          ),
+        ),
+      );
+    } else {
+      child = const SizedBox();
+    }
+
+    return AnimatedSwitcher(
+      duration: kShortAnimationDuration,
+      switchInCurve: Curves.easeInOut,
+      switchOutCurve: Curves.easeInOut,
+      child: child,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +96,13 @@ class SearchTextField extends StatelessWidget {
         borderRadius: BorderRadius.circular(128),
       ),
       child: TextField(
+        controller: _controller,
+        focusNode: _focusNode,
+        onSubmitted: widget.onSubmitted,
         decoration: InputDecoration(
           prefixIcon: const Icon(Icons.search),
+          suffixIcon: widget.onClear != null ? _buildClearIcon(theme) : null,
+          hintText: 'Search users',
           filled: true,
           fillColor: fillColor,
           isDense: true,
