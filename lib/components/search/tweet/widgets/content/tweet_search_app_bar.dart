48f17f3f4773371ac139a6664338cec2667da86a
Roberto Doering <rbydoering@gmail.com>
2021-01-30 16:20:29 +0100

    Add text to tweet search app bar

--- lib/components/search/tweet/widgets/content/tweet_search_app_bar.dart
+++ lib/components/search/tweet/widgets/content/tweet_search_app_bar.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harpy/components/common/misc/harpy_sliver_app_bar.dart';
+import 'package:harpy/components/search/tweet/bloc/tweet_search_bloc.dart';
 import 'package:harpy/components/search/widgets/search_text_field.dart';
 
 /// Builds a sliver app bar for the [TweetSearchScreen] with a [SearchTextField]
 /// in the title.
 class TweetSearchAppBar extends StatelessWidget {
-  const TweetSearchAppBar();
+  const TweetSearchAppBar({
+    this.text,
+  });
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
+    final TweetSearchBloc bloc = context.watch<TweetSearchBloc>();
+
     return HarpySliverAppBar(
       titleWidget: Container(
         child: SearchTextField(
+          text: text,
           hintText: 'search tweets',
-          onSubmitted: (String text) {}, // todo
+          onSubmitted: (String text) => bloc.add(
+            SearchTweets(query: text),
+          ), // todo
           onClear: () {}, // todo
         ),
       ),
