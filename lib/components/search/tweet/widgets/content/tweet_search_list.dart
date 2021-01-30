67dc63398a40dc59c47ecfe329b1adc93cc6f8c8
Roberto Doering <rbydoering@gmail.com>
2021-01-30 16:21:43 +0100

    Change tweet search list

--- lib/components/search/tweet/widgets/content/tweet_search_list.dart
+++ lib/components/search/tweet/widgets/content/tweet_search_list.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:harpy/components/common/list/load_more_listener.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harpy/components/common/list/scroll_direction_listener.dart';
 import 'package:harpy/components/common/list/scroll_to_start.dart';
+import 'package:harpy/components/search/tweet/bloc/tweet_search_bloc.dart';
 import 'package:harpy/components/search/tweet/widgets/content/tweet_search_app_bar.dart';
 import 'package:harpy/components/tweet/widgets/tweet_list.dart';
 import 'package:harpy/core/api/twitter/tweet_data.dart';
@@ -13,18 +14,19 @@ class TweetSearchList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TweetSearchBloc bloc = context.watch<TweetSearchBloc>();
+    final TweetSearchState state = bloc.state;
+
     return ScrollDirectionListener(
       child: ScrollToStart(
-        child: LoadMoreListener(
-          listen: false, // todo
-          onLoadMore: () async {}, // todo
-          child: const TweetList(
-            <TweetData>[],
-            enableScroll: false, // todo
-            beginSlivers: <Widget>[
-              TweetSearchAppBar(),
-            ],
-          ),
+        child: TweetList(
+          state is TweetSearchResult ? state.tweets : <TweetData>[],
+          enableScroll: state is TweetSearchResult,
+          beginSlivers: <Widget>[
+            TweetSearchAppBar(
+              text: state is TweetSearchResult ? state.searchQuery : null,
+            ),
+          ],
         ),
       ),
     );
