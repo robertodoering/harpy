c59bc7b350ccacf059a01931198c178aa100edf6
Roberto Doering <rbydoering@gmail.com>
2021-02-07 23:34:27 +0100

    Move home timeline to its own file

--- lib/components/timeline/home_timeline/widgets/home_screen.dart
+++ lib/components/timeline/home_timeline/widgets/home_screen.dart
@@ -3,23 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:harpy/components/common/dialogs/changelog_dialog.dart';
 import 'package:harpy/components/common/dialogs/harpy_exit_dialog.dart';
-import 'package:harpy/components/common/list/load_more_indicator.dart';
 import 'package:harpy/components/common/list/scroll_direction_listener.dart';
-import 'package:harpy/components/common/list/scroll_to_start.dart';
-import 'package:harpy/components/common/list/slivers/sliver_fill_loading_error.dart';
-import 'package:harpy/components/common/list/slivers/sliver_fill_loading_indicator.dart';
 import 'package:harpy/components/common/misc/harpy_scaffold.dart';
 import 'package:harpy/components/compose/widget/compose_screen.dart';
-import 'package:harpy/components/settings/layout/widgets/layout_padding.dart';
 import 'package:harpy/components/timeline/new/home_timeline/bloc/home_timeline_bloc.dart';
-import 'package:harpy/components/tweet/widgets/tweet_list.dart';
-import 'package:harpy/core/api/twitter/tweet_data.dart';
 import 'package:harpy/core/service_locator.dart';
 import 'package:harpy/misc/harpy_navigator.dart';
 
-import 'content/home_app_bar.dart';
 import 'content/home_drawer.dart';
-import 'content/new_tweets_text.dart';
+import 'content/home_timeline.dart';
 
 /// The home screen for an authenticated user.
 class HomeScreen extends StatefulWidget {
@@ -138,121 +130,6 @@ class _HomeScreenState extends State<HomeScreen> with RouteAware {
             //     },
             //   ),
             // ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class HomeTimeline extends StatefulWidget {
-  const HomeTimeline();
-
-  @override
-  _HomeTimelineState createState() => _HomeTimelineState();
-}
-
-class _HomeTimelineState extends State<HomeTimeline> {
-  ScrollController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = ScrollController();
-  }
-
-  void _blocListener(BuildContext context, HomeTimelineState state) {
-    if (state is HomeTimelineResult && state.initialResults) {
-      // scroll to the end after the list has been built
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        _controller.jumpTo(_controller.position.maxScrollExtent);
-      });
-    }
-  }
-
-  Widget _tweetBuilder(
-    HomeTimelineState state,
-    TweetData tweet,
-  ) {
-    bool newTweetsExist = false;
-    bool isLastInitialTweet = false;
-    bool includesLastVisibleTweet = false;
-
-    if (state is HomeTimelineResult) {
-      newTweetsExist = state.newTweetsExist;
-      isLastInitialTweet = state.lastInitialTweet == tweet.idStr;
-      includesLastVisibleTweet = state.includesLastVisibleTweet;
-    } else if (state is HomeTimelineLoadingOlder) {
-      newTweetsExist = state.oldResult.newTweetsExist;
-      isLastInitialTweet  = state.oldResult.lastInitialTweet == tweet.idStr;
-      includesLastVisibleTweet = state.oldResult.includesLastVisibleTweet;
-    }
-
-    if (newTweetsExist && isLastInitialTweet) {
-      final List<Widget> children = <Widget>[
-        TweetList.defaultTweetBuilder(tweet),
-        defaultVerticalSpacer,
-        const NewTweetsText(),
-      ];
-
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        // build the new tweets text above the last visible tweet if it exist
-        children: includesLastVisibleTweet
-            ? children.reversed.toList()
-            : children,
-      );
-    } else {
-      return TweetList.defaultTweetBuilder(tweet);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final MediaQueryData mediaQuery = MediaQuery.of(context);
-    final NewHomeTimelineBloc bloc = context.watch<NewHomeTimelineBloc>();
-    final HomeTimelineState state = bloc.state;
-
-    // todo: show text at the end of a timeline when no older tweets can be
-    //  retrieved
-
-    return BlocListener<NewHomeTimelineBloc, HomeTimelineState>(
-      listener: _blocListener,
-      child: ScrollDirectionListener(
-        child: ScrollToStart(
-          controller: _controller,
-          child: RefreshIndicator(
-            onRefresh: () async {},
-            child: TweetList(
-              state.timelineTweets,
-              controller: _controller,
-              tweetBuilder: (TweetData tweet) => _tweetBuilder(state, tweet),
-              beginSlivers: const <Widget>[
-                HomeAppBar(),
-              ],
-              endSlivers: <Widget>[
-                if (state.showInitialLoading)
-                  const SliverFillLoadingIndicator()
-                else if (state.showLoadingOlder)
-                  const LoadMoreIndicator()
-                  // todo: show end reached
-                else if (state.showNoTweetsFound)
-                  SliverFillLoadingError(
-                    message: const Text('no tweets found'),
-                    onRetry: () => bloc.add(null),
-                  )
-                else if (state.showSearchError)
-                  SliverFillLoadingError(
-                    message: const Text('error loading tweets'),
-                    onRetry: () => bloc.add(null),
-                  ),
-
-                SliverToBoxAdapter(
-                  child: SizedBox(height: mediaQuery.padding.bottom),
-                ),
-              ],
-            ),
           ),
         ),
       ),
