d0ccd2d2a0a46ad0ca13d031bbba27df2471e8a8
Roberto Doering <rbydoering@gmail.com>
2021-02-18 06:13:39 +0100

    Implement trends in search screen

--- lib/components/search/widgets/search_screen.dart
+++ lib/components/search/widgets/search_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:harpy/components/common/list/scroll_direction_listener.dart';
+import 'package:harpy/components/common/list/scroll_to_start.dart';
 import 'package:harpy/components/common/misc/harpy_scaffold.dart';
 import 'package:harpy/components/common/misc/harpy_sliver_app_bar.dart';
 import 'package:harpy/components/search/user/widgets/user_search_screen.dart';
 import 'package:harpy/components/settings/layout/widgets/layout_padding.dart';
+import 'package:harpy/components/trends/widgets/trends_list.dart';
 import 'package:harpy/core/service_locator.dart';
 import 'package:harpy/misc/harpy_navigator.dart';
 
@@ -41,31 +44,41 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MediaQueryData mediaQuery = MediaQuery.of(context);
+
     return HarpyScaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          const HarpySliverAppBar(
-            title: 'search',
-          ),
-          SliverPadding(
-            padding: DefaultEdgeInsets.all(),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate(
-                <Widget>[
-                  _buildUserSearchCard(),
-                  defaultVerticalSpacer,
-                  _buildTweetSearchCard(),
-                  defaultVerticalSpacer,
-                  const ListTile(
-                    leading: Icon(FeatherIcons.trendingUp, size: 18),
-                    title: Text('trends'),
-                  ),
-                  defaultVerticalSpacer,
-                ],
+      body: ScrollDirectionListener(
+        child: ScrollToStart(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              const HarpySliverAppBar(
+                title: 'search',
+                floating: true,
               ),
-            ),
-          )
-        ],
+              SliverPadding(
+                padding: DefaultEdgeInsets.all(),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    <Widget>[
+                      _buildUserSearchCard(),
+                      defaultVerticalSpacer,
+                      _buildTweetSearchCard(),
+                      defaultVerticalSpacer,
+                      const ListTile(
+                        leading: Icon(FeatherIcons.trendingUp, size: 18),
+                        title: Text('worldwide trends'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              TrendsList(),
+              SliverToBoxAdapter(
+                child: SizedBox(height: mediaQuery.padding.bottom),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
