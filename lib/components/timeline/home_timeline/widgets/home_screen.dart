74b00c185792df73f002edf0b7bbc759a84378b1
Roberto Doering <rbydoering@gmail.com>
2021-02-22 16:13:27 +0100

    Disable end drawer gesture opening

--- lib/components/timeline/home_timeline/widgets/home_screen.dart
+++ lib/components/timeline/home_timeline/widgets/home_screen.dart
@@ -95,6 +95,7 @@ class _HomeScreenState extends State<HomeScreen> with RouteAware {
           child: HarpyScaffold(
             drawer: const HomeDrawer(),
             endDrawer: const HomeTimelineFilterDrawer(),
+            endDrawerEnableOpenDragGesture: false,
             floatingActionButton: _buildFloatingActionButton(),
             body: HomeTimeline(),
           ),
