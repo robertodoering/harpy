ee289d7476413568d9dbb2cedf08f9ecd7e847c3
Roberto Doering <roberto.doering@aboutyou.com>
2021-04-04 11:32:57 +0200

    Update home floating action button

--- lib/components/common/timeline/home_timeline/widgets/home_timeline.dart
+++ lib/components/common/timeline/home_timeline/widgets/home_timeline.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:harpy/api/api.dart';
 import 'package:harpy/components/components.dart';
+import 'package:harpy/core/core.dart';
+import 'package:harpy/misc/misc.dart';
 
 class HomeTimeline extends StatefulWidget {
   // non-const to always rebuild when returning to home screen
@@ -76,43 +79,49 @@ class _HomeTimelineState extends State<HomeTimeline> {
               bloc.add(const RequestOlderHomeTimeline());
               await bloc.requestOlderCompleter.future;
             },
-            child: TweetList(
-              state.timelineTweets,
-              controller: _controller,
-              tweetBuilder: (TweetData tweet) => _tweetBuilder(state, tweet),
-              enableScroll: state.enableScroll,
-              endSlivers: <Widget>[
-                if (state.showInitialLoading)
-                  const SliverFillLoadingIndicator()
-                else if (state.showNoTweetsFound)
-                  SliverFillLoadingError(
-                    message: const Text('no tweets found'),
-                    onRetry: () => bloc.add(
-                      const RefreshHomeTimeline(clearPrevious: true),
+            child: ScrollAwareFloatingActionButton(
+              floatingActionButton: FloatingActionButton(
+                onPressed: () => app<HarpyNavigator>().pushComposeScreen(),
+                child: const Icon(FeatherIcons.feather, size: 28),
+              ),
+              child: TweetList(
+                state.timelineTweets,
+                controller: _controller,
+                tweetBuilder: (TweetData tweet) => _tweetBuilder(state, tweet),
+                enableScroll: state.enableScroll,
+                endSlivers: <Widget>[
+                  if (state.showInitialLoading)
+                    const SliverFillLoadingIndicator()
+                  else if (state.showNoTweetsFound)
+                    SliverFillLoadingError(
+                      message: const Text('no tweets found'),
+                      onRetry: () => bloc.add(
+                        const RefreshHomeTimeline(clearPrevious: true),
+                      ),
+                      onClearFilter: state.hasTimelineFilter
+                          ? () => bloc.add(const FilterHomeTimeline(
+                                timelineFilter: TimelineFilter.empty,
+                              ))
+                          : null,
+                    )
+                  else if (state.showTimelineError)
+                    SliverFillLoadingError(
+                      message: const Text('error loading tweets'),
+                      onRetry: () => bloc.add(
+                        const RefreshHomeTimeline(clearPrevious: true),
+                      ),
+                    )
+                  else if (state.showLoadingOlder)
+                    const SliverBoxLoadingIndicator()
+                  else if (state.showReachedEnd)
+                    const SliverBoxInfoMessage(
+                      secondaryMessage: Text('no more tweets available'),
                     ),
-                    onClearFilter: state.hasTimelineFilter
-                        ? () => bloc.add(const FilterHomeTimeline(
-                              timelineFilter: TimelineFilter.empty,
-                            ))
-                        : null,
-                  )
-                else if (state.showTimelineError)
-                  SliverFillLoadingError(
-                    message: const Text('error loading tweets'),
-                    onRetry: () => bloc.add(
-                      const RefreshHomeTimeline(clearPrevious: true),
-                    ),
-                  )
-                else if (state.showLoadingOlder)
-                  const SliverBoxLoadingIndicator()
-                else if (state.showReachedEnd)
-                  const SliverBoxInfoMessage(
-                    secondaryMessage: Text('no more tweets available'),
+                  SliverToBoxAdapter(
+                    child: SizedBox(height: mediaQuery.padding.bottom),
                   ),
-                SliverToBoxAdapter(
-                  child: SizedBox(height: mediaQuery.padding.bottom),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/components/screens/home/home_screen.dart
+++ lib/components/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:harpy/components/components.dart';
 import 'package:harpy/core/core.dart';
 import 'package:harpy/misc/misc.dart';
@@ -22,8 +21,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with RouteAware {
-  bool _showFab = true;
-
   @override
   void initState() {
     super.initState();
@@ -51,45 +48,23 @@ class _HomeScreenState extends State<HomeScreen> with RouteAware {
   @override
   void didPopNext() {
     // force a rebuild when the home screen shows again
+    // e.g. to rebuild after changing default padding to animate padding changes
     setState(() {});
-  }
-
-  void _onScrollDirectionChanged(VerticalDirection direction) {
-    final bool show = direction != VerticalDirection.down;
-
-    if (_showFab != show) {
-      setState(() {
-        _showFab = show;
-      });
-    }
-  }
-
-  Widget _buildFloatingActionButton() {
-    if (_showFab) {
-      return FloatingActionButton(
-        onPressed: () => app<HarpyNavigator>().pushComposeScreen(),
-        child: const Icon(FeatherIcons.feather, size: 28),
-      );
-    } else {
-      return null;
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopHarpy(
-      child: ScrollDirectionListener(
-        onScrollDirectionChanged: _onScrollDirectionChanged,
-        child: ChangeNotifierProvider<TimelineFilterModel>(
-          create: (_) => TimelineFilterModel.home(),
-          child: BlocProvider<TrendsBloc>(
-            create: (_) => TrendsBloc()..add(const FindTrendsEvent.global()),
-            child: HarpyScaffold(
-              drawer: const HomeDrawer(),
-              endDrawer: const HomeTimelineFilterDrawer(),
-              endDrawerEnableOpenDragGesture: false,
-              floatingActionButton: _buildFloatingActionButton(),
-              body: HomeTabView(),
+      child: ChangeNotifierProvider<TimelineFilterModel>(
+        create: (_) => TimelineFilterModel.home(),
+        child: BlocProvider<TrendsBloc>(
+          create: (_) => TrendsBloc()..add(const FindTrendsEvent.global()),
+          child: HarpyScaffold(
+            drawer: const HomeDrawer(),
+            endDrawer: const HomeTimelineFilterDrawer(),
+            endDrawerEnableOpenDragGesture: false,
+            body: ScrollDirectionListener(
+              child: HomeTabView(),
             ),
           ),
         ),
