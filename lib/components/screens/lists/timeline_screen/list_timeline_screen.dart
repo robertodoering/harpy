ca6add0174454bd8879c1f5a3fcb299516a36274
Roberto Doering <rbydoering@gmail.com>
2021-05-13 18:52:13 +0200

    Add action to list screen

--- lib/components/screens/lists/timeline_screen/list_timeline_screen.dart
+++ lib/components/screens/lists/timeline_screen/list_timeline_screen.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harpy/api/api.dart';
 import 'package:harpy/components/components.dart';
+import 'package:harpy/core/core.dart';
+import 'package:harpy/misc/harpy_navigator.dart';
 
 /// Shows a [ListTimeline] for the [list] in it's own screen.
 class ListTimelineScreen extends StatelessWidget {
@@ -12,6 +15,21 @@ class ListTimelineScreen extends StatelessWidget {
   final TwitterListData? list;
 
   static const String route = 'list_timeline_screen';
+
+  Widget _buildActionsButton(BuildContext context) {
+    return CustomPopupMenuButton<void>(
+      icon: const Icon(CupertinoIcons.ellipsis_vertical),
+      onSelected: (_) => app<HarpyNavigator>().pushListMemberScreen(list: list),
+      itemBuilder: (context) {
+        return <PopupMenuEntry<int>>[
+          const HarpyPopupMenuItem<int>(
+            value: 0,
+            text: Text('show members'),
+          ),
+        ];
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +44,9 @@ class ListTimelineScreen extends StatelessWidget {
                 HarpySliverAppBar(
                   title: list!.name,
                   floating: true,
+                  actions: [
+                    _buildActionsButton(context),
+                  ],
                 ),
               ],
             ),
