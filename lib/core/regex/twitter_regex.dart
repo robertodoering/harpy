56fa9ace9b477e71d96eb7fac58b93f7577ecc58
Roberto Doering <rbydoering@gmail.com>
2021-05-29 15:53:36 +0200

    Update twitter regex entity begin group

--- lib/core/regex/twitter_regex.dart
+++ lib/core/regex/twitter_regex.dart
@@ -1,32 +1,28 @@
 /// Positive lookbehind group, matches:
-/// * A special character (., 。)
-/// * Whitespaces
-/// * The beginning of the string
-const String _tagBeginGroup = r'(?<=(\.|。)|\s+|^)';
+/// * Any non-word characters (\W+)
+/// * An underscore (_)
+/// * The beginning of the string (^)
+const String _tagBeginGroup = r'(?<=\W+|_|^)';
 
 final RegExp hashtagRegex = RegExp(
-  // ignore: prefer_interpolation_to_compose_strings
   _tagBeginGroup + r'(?=.{2,140})(#|＃){1}([0-9_\p{L}]*[_\p{L}][0-9_\p{L}]*)',
   caseSensitive: false,
   unicode: true,
 );
 
 final RegExp hashtagStartRegex = RegExp(
-  // ignore: prefer_interpolation_to_compose_strings
-  _tagBeginGroup + '(#|＃){1}',
+  '$_tagBeginGroup(#|＃){1}',
   caseSensitive: false,
   unicode: true,
 );
 
 final RegExp mentionRegex = RegExp(
-  // ignore: prefer_interpolation_to_compose_strings
   _tagBeginGroup + r'(@{1}\w+)',
   caseSensitive: false,
 );
 
 final RegExp mentionStartRegex = RegExp(
-  // ignore: prefer_interpolation_to_compose_strings
-  _tagBeginGroup + '(@{1})',
+  '$_tagBeginGroup(@{1})',
   caseSensitive: false,
 );
 
