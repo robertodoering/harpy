58ae11eca00e380008893b0401b03ba12779cf14
Goooler <wangzongler@gmail.com>
2021-08-12 06:10:59 +0800

    Reformat codes with indent size 2 (#407)
    * Reformat codes under android folder with indent size 2
    
    * Reformat all yml files
    
    * Reformat gradle

--- android/app/src/main/java/com/robertodoering/harpy/MainActivity.java
+++ android/app/src/main/java/com/robertodoering/harpy/MainActivity.java
@@ -8,20 +8,20 @@ import androidx.annotation.Nullable;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        final View view = findViewById(android.R.id.content);
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    final View view = findViewById(android.R.id.content);
 
-        if (view != null) {
-            view.setSystemUiVisibility(
-                // Tells the system that the window wishes the content to
-                // be laid out at the most extreme scenario.
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                // Tells the system that the window wishes the content to
-                // be laid out as if the navigation bar was hidden
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            );
-        }
+    if (view != null) {
+      view.setSystemUiVisibility(
+        // Tells the system that the window wishes the content to
+        // be laid out at the most extreme scenario.
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+          // Tells the system that the window wishes the content to
+          // be laid out as if the navigation bar was hidden
+          View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      );
     }
+  }
 }
