7fb0b4a577cd8f74abbdbb0bdef1302466a7a566
Goooler <wangzongler@gmail.com>
2021-09-11 19:51:04 +0800

    Replace deprecated methods

--- android/app/src/main/java/com/robertodoering/harpy/AndroidPathProviderPlugin.kt
+++ android/app/src/main/java/com/robertodoering/harpy/AndroidPathProviderPlugin.kt
@@ -1,5 +1,6 @@
 package com.robertodoering.harpy
 
+import android.content.Context
 import android.os.Environment
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -7,51 +8,45 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 class AndroidPathProviderPlugin : FlutterPlugin, MethodCallHandler {
+  private lateinit var context: Context
 
-  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) = Unit
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    context = binding.applicationContext
+  }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) = Unit
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     when (call.method) {
       "getAlarmsPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_ALARMS)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_ALARMS)?.absolutePath
       )
       "getDCIMPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_DCIM)?.absolutePath
       )
       "getDocumentsPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath
       )
       "getDownloadsPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
       )
       "getMoviesPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.absolutePath
       )
       "getMusicPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath
       )
       "getNotificationsPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_NOTIFICATIONS)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_NOTIFICATIONS)?.absolutePath
       )
       "getPicturesPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath
       )
       "getPodcastsPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PODCASTS)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_PODCASTS)?.absolutePath
       )
       "getRingtonesPath" -> result.success(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_RINGTONES)
-          .absolutePath
+        context.getExternalFilesDir(Environment.DIRECTORY_RINGTONES)?.absolutePath
       )
       else -> result.notImplemented()
     }
