37d457128ca2b1566e226ce09e79c4a824a0d4c2
Roberto Doering <rbydoering@gmail.com>
2021-10-25 20:57:41 +0200

    Fix export

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -116,7 +116,6 @@ export 'screens/home/widgets/home_lists_provider.dart';
 export 'screens/home/widgets/home_tab_bar.dart';
 export 'screens/home/widgets/home_timeline_filter_drawer.dart';
 export 'screens/home/widgets/home_top_padding.dart';
-export 'screens/home/widgets/mentions_tab.dart';
 export 'screens/home/widgets/new_tweets_text.dart';
 export 'screens/lists/members/bloc/list_members_bloc.dart';
 export 'screens/lists/members/list_members_screen.dart';
