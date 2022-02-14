63bcb4a507ea4fe8769bfe948b96f1f1be161bb6
Roberto Doering <rbydoering@gmail.com>
2022-02-14 02:06:21 +0100

    Update splash screen

--- lib/components/splash/splash_page.dart
+++ lib/components/splash/splash_page.dart
@@ -1,22 +1,56 @@
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:harpy/components/components.dart';
+import 'package:harpy/core/core.dart';
 
-class SplashPage extends ConsumerWidget {
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage();
 
   static const name = 'splash';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const HarpyBackground(
-      child: FractionallySizedBox(
-        widthFactor: 0.66,
-        child: FlareActor(
-          'assets/flare/harpy_logo.flr',
-          animation: 'show',
-        ),
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends ConsumerState<SplashPage> {
+  bool _showLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _startTimer();
+  }
+
+  Future<void> _startTimer() async {
+    await Future<void>.delayed(const Duration(seconds: 2));
+
+    if (mounted) {
+      setState(() => _showLoading = true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return HarpyBackground(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: size.height / 2,
+            child: const FractionallySizedBox(
+              widthFactor: 0.66,
+              child: FlareAnimation.harpyLogo(animation: 'show'),
+            ),
+          ),
+          AnimatedOpacity(
+            opacity: _showLoading ? 1 : 0,
+            duration: kLongAnimationDuration,
+            child: const CircularProgressIndicator(),
+          ),
+        ],
       ),
     );
   }
