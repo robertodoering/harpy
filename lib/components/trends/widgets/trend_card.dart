a93e2a7623b5dd15b627c19336bd8175eaa527bb
Roberto Doering <rbydoering@gmail.com>
2022-02-25 20:43:10 +0100

    Cleanup

--- lib/components/trends/widgets/trend_card.dart
+++ lib/components/trends/widgets/trend_card.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:harpy/components/components.dart';
-import 'package:harpy/core/core.dart';
 import 'package:harpy/rby/rby.dart';
 import 'package:intl/intl.dart';
 
@@ -18,8 +17,6 @@ class TrendCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(routerProvider);
-
     return VisibilityChangeListener(
       detectorKey: ValueKey(trend.hashCode),
       child: ListCardAnimation(
