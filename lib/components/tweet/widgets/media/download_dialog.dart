c57331e375e4e9210bc257fdd51506c5cfba3e71
Roberto Doering <rbydoering@gmail.com>
2022-03-28 20:41:25 +0200

    Move filename editing controller

--- lib/components/tweet/widgets/media/download_dialog.dart
+++ lib/components/tweet/widgets/media/download_dialog.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:harpy/api/api.dart';
 import 'package:harpy/components/components.dart';
-import 'package:harpy/core/core.dart';
+import 'package:harpy/rby/rby.dart';
 
 class DownloadDialog extends ConsumerStatefulWidget {
   const DownloadDialog({
--- lib/rby/rby.dart
+++ lib/rby/rby.dart
@@ -9,6 +9,7 @@ export 'animations/immediate/immediate_scale_animation.dart';
 export 'animations/immediate/immediate_slide_animation.dart';
 export 'animations/implicit/animated_number.dart';
 export 'core/converter/built_list_converter.dart';
+export 'core/filename_editing_controller.dart';
 export 'core/handle_error_extension.dart';
 export 'core/log_error_handler.dart';
 export 'core/logger.dart';
