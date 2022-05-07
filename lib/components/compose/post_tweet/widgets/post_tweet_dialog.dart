3956caeac727006ac1c7a3f9e9fdb121bd707322
Roberto Doering <rbydoering@gmail.com>
2022-05-07 15:01:43 +0200

    Cleanup

--- lib/components/compose/post_tweet/widgets/post_tweet_dialog.dart
+++ lib/components/compose/post_tweet/widgets/post_tweet_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
