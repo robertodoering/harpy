dcc14c3e8d6b1150e87a29be5603c02ab1de5c7d
Roberto Doering <rbydoering@gmail.com>
2022-05-07 23:37:47 +0200

    Animate trends list state change

--- lib/components/trends/widgets/trends_list.dart
+++ lib/components/trends/widgets/trends_list.dart
@@ -3,6 +3,8 @@ import 'package:dart_twitter_api/twitter_api.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:harpy/components/components.dart';
+import 'package:harpy/core/harpy_theme/harpy_theme.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 class TrendsList extends ConsumerWidget {
   const TrendsList();
@@ -15,24 +17,27 @@ class TrendsList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final display = ref.watch(displayPreferencesProvider);
 
-    return ref.watch(trendsProvider).when(
-          loading: () => const TrendsListLoadingSliver(),
-          data: (state) => state.trends.isNotEmpty
-              ? SliverPadding(
-                  padding: display.edgeInsets.copyWith(top: 0),
-                  sliver: SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (_, index) => _itemBuilder(index, state.trends),
-                      childCount: state.trends.length * 2 - 1,
+    return SliverAnimatedSwitcher(
+      duration: kShortAnimationDuration,
+      child: ref.watch(trendsProvider).when(
+            loading: () => const TrendsListLoadingSliver(),
+            data: (state) => state.trends.isNotEmpty
+                ? SliverPadding(
+                    padding: display.edgeInsets.copyWith(top: 0),
+                    sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (_, index) => _itemBuilder(index, state.trends),
+                        childCount: state.trends.length * 2 - 1,
+                      ),
                     ),
+                  )
+                : const SliverFillInfoMessage(
+                    secondaryMessage: Text('no trends'),
                   ),
-                )
-              : const SliverFillInfoMessage(
-                  secondaryMessage: Text('no trends'),
-                ),
-          error: (_, __) => const SliverFillInfoMessage(
-            secondaryMessage: Text('error requesting trends'),
+            error: (_, __) => const SliverFillInfoMessage(
+              secondaryMessage: Text('error requesting trends'),
+            ),
           ),
-        );
+    );
   }
 }
--- lib/components/widgets/harpy_animated_switcher.dart
+++ lib/components/widgets/harpy_animated_switcher.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:harpy/core/harpy_theme/harpy_theme.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 /// Builds an [AnimatedSwitcher] with some predefined defaults.
 class HarpyAnimatedSwitcher extends StatelessWidget {
@@ -7,12 +8,21 @@ class HarpyAnimatedSwitcher extends StatelessWidget {
     required this.child,
     this.duration = kShortAnimationDuration,
     this.reverseDuration,
+    this.transitionBuilder = AnimatedSwitcher.defaultTransitionBuilder,
     this.layoutBuilder = AnimatedSwitcher.defaultLayoutBuilder,
   });
+
+  const HarpyAnimatedSwitcher.sliver({
+    required this.child,
+    this.duration = kShortAnimationDuration,
+    this.reverseDuration,
+  })  : transitionBuilder = SliverAnimatedSwitcher.defaultTransitionBuilder,
+        layoutBuilder = SliverAnimatedSwitcher.defaultLayoutBuilder;
 
   final Widget child;
   final Duration duration;
   final Duration? reverseDuration;
+  final AnimatedSwitcherTransitionBuilder transitionBuilder;
   final AnimatedSwitcherLayoutBuilder layoutBuilder;
 
   @override
