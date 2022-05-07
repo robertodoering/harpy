3482de474fc50d27b00c33fddab5e82b5f56207e
Roberto Doering <rbydoering@gmail.com>
2022-05-07 20:43:54 +0200

    Cleanup

--- lib/components/trends/find_trends_location/widgets/found_trends_locations.dart
+++ lib/components/trends/find_trends_location/widgets/found_trends_locations.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:harpy/components/components.dart';
-import 'package:harpy/core/core.dart';
 
 class FoundTrendsLocations extends ConsumerWidget {
   const FoundTrendsLocations();
