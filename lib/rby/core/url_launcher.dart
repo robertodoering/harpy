53de56303b324fb4d7c99732ab239d749b40a775
Roberto Doering <rbydoering@gmail.com>
2022-05-12 22:40:44 +0200

    Update url launcher

--- lib/rby/core/url_launcher.dart
+++ lib/rby/core/url_launcher.dart
@@ -3,7 +3,10 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> safeLaunchUrl(String url) async {
   try {
-    await launchUrl(Uri.parse(url));
+    await launchUrl(
+      Uri.parse(url),
+      mode: LaunchMode.externalApplication,
+    );
   } catch (e) {
     Logger('UrlLauncher').warning('cant launch url $url', e);
   }
