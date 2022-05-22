b1d380655b3bd0674f5d918afc440a13836df401
Roberto Doering <rbydoering@gmail.com>
2022-05-22 13:30:13 +0200

    Add support for in-app markdown links

--- lib/components/widgets/harpy_markdown.dart
+++ lib/components/widgets/harpy_markdown.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:harpy/core/core.dart';
 
-class HarpyMarkdown extends StatelessWidget {
+class HarpyMarkdown extends ConsumerWidget {
   const HarpyMarkdown(this.data);
 
   final String data;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
 
     return Markdown(
@@ -15,6 +17,7 @@ class HarpyMarkdown extends StatelessWidget {
       padding: EdgeInsets.zero,
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
+      onTapLink: (_, href, __) => _onLinkTap(context, ref.read, href: href),
       styleSheet: MarkdownStyleSheet(
         code: theme.textTheme.bodyText2!.copyWith(
           backgroundColor: theme.colorScheme.onBackground.withOpacity(.07),
@@ -24,4 +27,12 @@ class HarpyMarkdown extends StatelessWidget {
       ),
     );
   }
+}
+
+void _onLinkTap(BuildContext context, Reader read, {required String? href}) {
+  if (href == null) return;
+
+  final router = read(routerProvider);
+
+  if (router.location.startsWith('/home')) router.go(href);
 }
