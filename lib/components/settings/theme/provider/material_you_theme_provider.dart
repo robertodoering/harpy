1185ed35aaa15dc587bd233fecb1867dbdbff869
Roberto Doering <rbydoering@gmail.com>
2022-06-23 16:20:18 +0200

    Add material you themes (#696)
    * Add material you themes
    
    * Cleanup
    
    * Add material you themes to setup page
    
    * Update material you theme names
    
    * Update changelog
    
    * Cleanup

---
+++ lib/components/settings/theme/provider/material_you_theme_provider.dart
@@ -0,0 +1,40 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:harpy/core/core.dart';
+import 'package:harpy/rby/rby.dart';
+
+final corePaletteProvider = FutureProvider(
+  (ref) => DynamicColorPlugin.getCorePalette().handleError(logErrorHandler),
+  name: 'CorePaletteProvider',
+);
+
+final materialYouLightProvider = FutureProvider(
+  (ref) async {
+    final corePalette = ref.watch(corePaletteProvider).asData?.value;
+
+    return corePalette != null
+        ? HarpyThemeData.fromCorePalette(
+            corePalette,
+            brightness: Brightness.light,
+            name: 'Material You · light',
+          )
+        : null;
+  },
+  name: 'MaterialYouLightProvider',
+);
+
+final materialYouDarkProvider = FutureProvider(
+  (ref) async {
+    final corePalette = ref.watch(corePaletteProvider).asData?.value;
+
+    return corePalette != null
+        ? HarpyThemeData.fromCorePalette(
+            corePalette,
+            brightness: Brightness.dark,
+            name: 'Material You · dark',
+          )
+        : null;
+  },
+  name: 'MaterialYouDarkProvider',
+);
--- lib/core/harpy_theme/harpy_theme_data.dart
+++ lib/core/harpy_theme/harpy_theme_data.dart
@@ -1,4 +1,7 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:material_color_utilities/material_color_utilities.dart';
 
 part 'harpy_theme_data.freezed.dart';
 part 'harpy_theme_data.g.dart';
@@ -14,6 +17,28 @@ class HarpyThemeData with _$HarpyThemeData {
     required int statusBarColor,
     required int navBarColor,
   }) = _HarpyThemeData;
+
+  /// Creates the [HarpyThemeData] based on the [CorePalette] used by Material
+  /// You.
+  factory HarpyThemeData.fromCorePalette(
+    CorePalette palette, {
+    required Brightness brightness,
+    required String name,
+  }) {
+    final colorScheme = palette.toColorScheme(brightness: brightness)!;
+
+    return HarpyThemeData(
+      name: name,
+      backgroundColors: [colorScheme.background.value],
+      primaryColor: colorScheme.primary.value,
+      secondaryColor: colorScheme.tertiary.value,
+      cardColor: brightness == Brightness.dark
+          ? Color(palette.neutral.get(30)).withOpacity(.3).value
+          : Color(palette.neutral.get(80)).withOpacity(.3).value,
+      statusBarColor: colorScheme.background.withOpacity(0).value,
+      navBarColor: colorScheme.background.withOpacity(0).value,
+    );
+  }
 
   factory HarpyThemeData.fromJson(Map<String, dynamic> json) =>
       _$HarpyThemeDataFromJson(json);
