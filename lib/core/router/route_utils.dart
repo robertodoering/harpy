a3856bf5072e0491bd514e2f42e0f4f8a644cef2
Roberto Doering <rbydoering@gmail.com>
2022-10-05 23:36:03 +0200

    Revert 'Update go_router'

--- lib/core/router/route_utils.dart
+++ lib/core/router/route_utils.dart
@@ -1,8 +1,6 @@
 import 'package:go_router/go_router.dart';
 // ignore: implementation_imports
-import 'package:go_router/src/configuration.dart';
-// ignore: implementation_imports
-import 'package:go_router/src/match.dart';
+import 'package:go_router/src/go_route_match.dart';
 
 /// Whether a given location has a matching route.
 bool locationHasRouteMatch({
@@ -11,94 +9,90 @@ bool locationHasRouteMatch({
 }) {
   final uri = Uri.parse(location);
 
-  final result = _getLocRouteRecursively(
+  final result = _getLocationRoutes(
     loc: uri.path,
     restLoc: uri.path,
     routes: routes,
     parentFullpath: '',
     parentSubloc: '',
     queryParams: uri.queryParameters,
-    queryParametersAll: uri.queryParametersAll,
     extra: null,
   );
 
   return result.isNotEmpty;
 }
 
-List<RouteMatch> _getLocRouteRecursively({
+List<GoRouteMatch> _getLocationRoutes({
   required String loc,
   required String restLoc,
   required String parentSubloc,
-  required List<RouteBase> routes,
+  required List<GoRoute> routes,
   required String parentFullpath,
   required Map<String, String> queryParams,
-  required Map<String, List<String>> queryParametersAll,
   required Object? extra,
 }) {
-  final result = <List<RouteMatch>>[];
+  final result = <List<GoRouteMatch>>[];
 
   // find the set of matches at this level of the tree
   for (final route in routes) {
-    late final String fullpath;
+    final fullpath = _concatenatePaths(parentFullpath, route.path);
 
-    if (route is GoRoute) {
-      fullpath = _concatenatePaths(parentFullpath, route.path);
-    } else if (route is ShellRoute) {
-      fullpath = parentFullpath;
-    }
-
-    final match = RouteMatch.match(
+    final match = GoRouteMatch.match(
       route: route,
       restLoc: restLoc,
       parentSubloc: parentSubloc,
       fullpath: fullpath,
       queryParams: queryParams,
-      queryParametersAll: queryParametersAll,
       extra: extra,
     );
 
-    if (match == null) continue;
+    if (match == null) {
+      continue;
+    }
 
-    if (match.route is GoRoute &&
-        match.subloc.toLowerCase() == loc.toLowerCase()) {
+    if (match.subloc.toLowerCase() == loc.toLowerCase()) {
+      // If it is a complete match, then return the matched route
+      // NOTE: need a lower case match because subloc is canonicalized to match
+      // the path case whereas the location can be of any case and still match
       result.add([match]);
     } else if (route.routes.isEmpty) {
+      // If it is partial match but no sub-routes, bail.
       continue;
     } else {
-      final String childRestLoc;
-      final String newParentSubLoc;
+      // otherwise recurse
+      final childRestLoc = loc.substring(
+        match.subloc.length + (match.subloc == '/' ? 0 : 1),
+      );
 
-      if (match.route is ShellRoute) {
-        childRestLoc = restLoc;
-        newParentSubLoc = parentSubloc;
-      } else {
-        childRestLoc = loc.substring(
-          match.subloc.length + (match.subloc == '/' ? 0 : 1),
-        );
-        newParentSubLoc = match.subloc;
-      }
-
-      final subRouteMatch = _getLocRouteRecursively(
+      final subRouteMatch = _getLocationRoutes(
         loc: loc,
         restLoc: childRestLoc,
-        parentSubloc: newParentSubLoc,
+        parentSubloc: match.subloc,
         routes: route.routes,
         parentFullpath: fullpath,
         queryParams: queryParams,
-        queryParametersAll: queryParametersAll,
         extra: extra,
-      ).toList();
+      );
 
-      if (subRouteMatch.isEmpty) continue;
-
-      result.add([match, ...subRouteMatch]);
+      // if there's no sub-route matches, there is no match for this
+      // location
+      if (subRouteMatch.isEmpty) {
+        continue;
+      }
+      result.add(<GoRouteMatch>[match, ...subRouteMatch]);
     }
 
     break;
   }
 
-  if (result.isEmpty) return [];
+  if (result.isEmpty) {
+    return <GoRouteMatch>[];
+  }
 
+  // If there are multiple routes that match the location, returning the first
+  // one.
+  // To make predefined routes to take precedence over dynamic routes eg. '/:id'
+  // consider adding the dynamic route at the end of the routes
   return result.first;
 }
 
