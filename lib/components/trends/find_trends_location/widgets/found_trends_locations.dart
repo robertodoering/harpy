209267617d86db6131281ed6d8267782437897e6
Roberto Doering <rbydoering@gmail.com>
2022-10-17 21:29:49 +0200

    Cleanup

--- lib/components/trends/find_trends_location/widgets/found_trends_locations.dart
+++ lib/components/trends/find_trends_location/widgets/found_trends_locations.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
